da2300284cbdd3c44ff11f4ba9ca6bccd63db4bb
Daniel-W1 <82764803+Daniel-W1@users.noreply.github.com>
2022-02-22 23:20:32 +0300

    Time: 36 ms (83.15%), Space: 13.9 MB (80.10%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = []
+        min_leng = self.smallest(strs)
+        print(min_leng)
+        for i in range(min_leng):
+            check = strs[0][:i+1]
+            for word in strs:
+                if not word.startswith(check):
+                    return prefix[-1] if prefix else ""
+            prefix.append(check)
+        return prefix[-1] if prefix else ""
+                    
+    def smallest(self, strs):
+        min_leng = 201
+        for word in strs:
+            min_leng = min(min_leng, len(word))
+        return min_leng
+        
+        
