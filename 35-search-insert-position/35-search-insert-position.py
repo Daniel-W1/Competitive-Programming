f831611a1dad0f2faf8b8c69f9dc9949f081076a
Daniel-W1 <82764803+Daniel-W1@users.noreply.github.com>
2022-02-24 14:18:06 +0300

    Time: 53 ms (70.95%), Space: 14.9 MB (57.31%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,17 +1,35 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        temp = nums
-        while len(nums) > 1:
-            mid = len(nums)//2
-            if target == nums[mid]:
-                return temp.index(nums[mid])
-            else:
-                if target < nums[mid]: nums = nums[:mid]
-                else: 
-                    nums = nums[mid:]
+#         temp = nums
+#         while len(nums) > 1:
+#             mid = len(nums)//2
+#             if target == nums[mid]:
+#                 return temp.index(nums[mid])
+#             else:
+#                 if target < nums[mid]: nums = nums[:mid]
+#                 else: 
+#                     nums = nums[mid:]
         
-        if target > nums[0]: return temp.index(nums[0]) + 1
-        else: return temp.index(nums[0])
+#         if target > nums[0]: return temp.index(nums[0]) + 1
+#         else: return temp.index(nums[0])
+        l = 0 
+        h = len(nums)-1
+        while l < h and h-l > 1:
+            mid = (l+h)//2
+            if nums[mid] == target:
+                return mid
+            else:
+                if nums[mid] < target:
+                    l = mid
+                else: h = mid
+        if target <= nums[l]: return l
+        elif target > nums[l] and target <= nums[h]: return h
+        elif target == nums[l]: return l
+        else: return h+1
+        
+        
+               
+            
             
         
        
