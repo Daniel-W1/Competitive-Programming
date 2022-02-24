fee83f447b6dbf66d59b43ce40ca10e69fb2e1ef
Daniel-W1 <82764803+Daniel-W1@users.noreply.github.com>
2022-02-24 16:41:23 +0300

    Time: 50 ms (59.34%), Space: 14.2 MB (89.64%) - LeetHub

--- 274-h-index/274-h-index.py
+++ 274-h-index/274-h-index.py
@@ -1,16 +1,20 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         the_max = len(citations)
-        
+        citations= sorted(citations, reverse = True)
         if len(citations)==1:
             if citations[0] == 0: return 0
             else: return 1
             
         for check in range(the_max, -1, -1):
             cnt = 0
-            for val in citations[::-1]:
+            for val in citations:
                 if check <= val and cnt < check: 
                     cnt += 1
+                else:
+                    if cnt == check:
+                        return check
+                    break
                 if cnt == check:
                     return check
             
