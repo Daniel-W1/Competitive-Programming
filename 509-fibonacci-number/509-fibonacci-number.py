e6b5fdc810b8147b0ced484b18953b330b8319d3
Daniel-W1 <82764803+Daniel-W1@users.noreply.github.com>
2022-03-16 13:04:26 +0300

    Time: 45 ms (59.33%), Space: 13.8 MB (68.60%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -8,4 +8,7 @@ class Solution:
             for i in range(2, len(ans)):
                 ans[i] = ans[i-2]+ans[i-1]
         return ans[-1]
+    # the above code is an example of dp and 
+    # we can do this problem with naive recursion but that would take a ton of time
+    # the time complexity of naive rec and dp is 2^n , O(n) respectively
             
