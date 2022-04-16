54ab8fd82402603bba9a9c76af6bad91800d5ba3
Daniel-W1 <82764803+Daniel-W1@users.noreply.github.com>
2022-04-16 20:17:24 +0300

    Time: 32 ms (85.31%), Space: 13.8 MB (73.97%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,48 +1,27 @@
-# class Solution:
-#     def decodeString(self, s: str) -> str:
-#         stack = []
-#         ans = []
-#         for letter in s:
-#             if letter != "]":
-#                 stack.append(letter)
-#             else:
-#                 temp = []
-#                 while stack[-1] != "[":
-#                     temp.append(stack.pop())
-#                 stack.pop()
-#                 if stack: 
-#                     num = ""
-#                     while stack and stack[-1].isnumeric():
-#                         num += (stack.pop())
-#                     num = int(num[::-1])
-#                     temp = temp*num
-#                 stack += temp[::-1]
-                
-#         return "".join(stack)
-                    
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack = deque()
-        for i in s:
-            if i == ']':
-                word = stack.pop()
-                stack.pop()
-                k = stack.pop()
-                if not stack or stack[-1] in '[]':
-                    stack.append(word * int(k))
-                else:
-                    stack[-1] += word * int(k)
-            elif i == '[':
-                stack.append(i)
+        stack = []
+        ans = []
+        for letter in s:
+            if letter != "]":
+                stack.append(letter)
             else:
-                if stack and i.isdigit() == stack[-1].isdigit():
-                    if stack[-1] in '[]':
-                        stack.append(i)
-                    else:
-                        stack[-1] += i
-                else:
-                    stack.append(i)
+                temp = []
+                while stack[-1] != "[":
+                    temp.append(stack.pop())
+                stack.pop()
+                if stack: 
+                    num = ""
+                    while stack and stack[-1].isnumeric():
+                        num += (stack.pop())
+                    num = int(num[::-1])
+                    temp = temp*num
+                stack += temp[::-1]
+                
         return "".join(stack)
+                    
+                    
+            
                 
             
             
