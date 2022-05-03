ab744e0474f4046aa3583daba8fd234acebda948
Daniel-W1 <82764803+Daniel-W1@users.noreply.github.com>
2022-05-03 09:28:32 +0300

    Time: 2440 ms (43.04%), Space: 29.9 MB (61.21%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -2,23 +2,23 @@ class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         h = 0
         ans = []
-        minq = collections.deque([])
+        maxq = collections.deque([])
         while h != k:
-            while minq and nums[h] > minq[-1][0]:
-                minq.pop()
-            minq.append((nums[h], h))
+            while maxq and nums[h] > maxq[-1][0]:
+                maxq.pop()
+            maxq.append((nums[h], h))
             h += 1
-        ans.append(minq[0][0])
-        print(minq)
+        ans.append(maxq[0][0])
+        
         l=0
         while h < len(nums):
-            while minq and nums[h] > minq[-1][0]:
-                minq.pop()
-            minq.append((nums[h], h))
+            while maxq and nums[h] > maxq[-1][0]:
+                maxq.pop()
+            maxq.append((nums[h], h))
             l += 1
-            if l-1 == minq[0][1]:
-                minq.popleft()
-            ans.append(minq[0][0])
+            if l-1 == maxq[0][1]:
+                maxq.popleft()
+            ans.append(maxq[0][0])
             h += 1
         return ans
             
