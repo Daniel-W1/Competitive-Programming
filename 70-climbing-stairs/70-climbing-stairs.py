e634e6b201e234cbfb6d8203e6cfa3615271b9d4
Daniel-W1 <82764803+Daniel-W1@users.noreply.github.com>
2022-05-04 19:13:19 +0300

    Time: 36 ms (66.73%), Space: 13.8 MB (96.61%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        memo[1] = 1
+        memo[2] = 2
+        def climb(n):
+            if n in memo: 
+                return memo[n]
+            else:   
+                memo[n] =  climb(n-1) + climb(n-2)
+                return memo[n]
+        return climb(n)
+
+            
+            
