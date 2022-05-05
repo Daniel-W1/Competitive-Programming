3b61bf1b783e0cfab304114ceb033081233c8a25
Daniel-W1 <82764803+Daniel-W1@users.noreply.github.com>
2022-05-05 15:47:38 +0300

    Time: 30 ms (86.92%), Space: 13.8 MB (59.46%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,15 +1,23 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = {}
-        memo[1] = 1
-        memo[2] = 2
-        def climb(n):
-            if n in memo:
-                return memo[n]
-            else:   
-                memo[n] =  climb(n-1) + climb(n-2)
-                return memo[n]
-        return climb(n)
+        # memo = {}
+        # memo[1] = 1
+        # memo[2] = 2
+        # def climb(n):
+        #     if n in memo:
+        #         return memo[n]
+        #     else:   
+        #         memo[n] =  climb(n-1) + climb(n-2)
+        #         return memo[n]
+        # return climb(n)
+        if n == 1:
+            return 1
+        a, b = 1, 2
+        for i in range(2, n):
+            tmp = b
+            b = a+b
+            a = tmp
+        return b
 
             
             
