a5f185cdf71dd8c9b64ad6e7536f7a112e4b1469
Daniel-W1 <82764803+Daniel-W1@users.noreply.github.com>
2022-05-05 15:46:08 +0300

    Time: 40 ms (53.80%), Space: 14 MB (13.01%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -4,7 +4,7 @@ class Solution:
         memo[1] = 1
         memo[2] = 2
         def climb(n):
-            if n in memo: 
+            if n in memo:
                 return memo[n]
             else:   
                 memo[n] =  climb(n-1) + climb(n-2)
