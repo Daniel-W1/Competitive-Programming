dde79b49ae1595d389b2ed38ea99ca9144b001e7
Daniel-W1 <82764803+Daniel-W1@users.noreply.github.com>
2022-05-09 17:08:54 +0300

    Time: 1299 ms (48.03%), Space: 27.7 MB (44.43%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.py
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.py
@@ -1,8 +1,5 @@
 class Solution:
     def getDescentPeriods(self, prices: List[int]) -> int:
-        #[-1, -1, -1, 1]
-        #[8,6,7,7]
-        #[3,2,1,5,4,3,7,6,1,1,1]
         ranges = []
         l, h = 0,1
         p = len(prices)
@@ -12,9 +9,8 @@ class Solution:
             while h < len(prices) and prev-prices[h] == 1:
                 prev = prices[h]
                 h += 1
-            others.append(h-l)
+            val = h-l
+            p += (val**2 -((val-1)/2 + (val-1)**2/2))-val
             l = h
             h += 1
-        for val in others:
-            p += (val**2 -((val-1)/2 + (val-1)**2/2))-val
         return int(p)
