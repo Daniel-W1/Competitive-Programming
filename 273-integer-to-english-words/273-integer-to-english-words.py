21f874a02625f81950d8d99eab9b273cfb5dd85a
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-05-11 16:05:34 +0300

    Time: 40 ms (68.56%), Space: 13.9 MB (96.42%) - LeetHub

--- 273-integer-to-english-words/273-integer-to-english-words.py
+++ 273-integer-to-english-words/273-integer-to-english-words.py
@@ -1,33 +1,43 @@
 class Solution:
+    def convertThreeDigit(self, num: int) -> str:
+        nums = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
+                'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 
+                'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen',
+                'Nineteen']
+        tys = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy',
+               'Eighty', 'Ninety']
+        
+        res = ''
+        if num >= 100:
+            res += nums[num//100] + ' Hundred'
+            num = num%100
+        
+        if 1 <= num <= 19:
+            if res:
+                res += ' '
+            res += nums[num]
+        elif num >= 20:
+            if res:
+                res += ' '
+            res += tys[(num-20)//10]
+            num = (num-20)%10
+            if num:
+                res += ' ' + nums[num]
+        
+        return res if res else 'Zero'
+    
     def numberToWords(self, num: int) -> str:
-        if not num: return 'Zero'
-        ones = {1:' One', 2:' Two', 3:' Three', 4:' Four', 5:' Five', 6:' Six', 7:' Seven', 8:' Eight', 9:' Nine', 
-				10:' Ten', 11:' Eleven', 12:' Twelve', 13:' Thirteen', 14:' Fourteen', 15:' Fifteen', 16:' Sixteen', 
-				17:' Seventeen', 18:' Eighteen', 19:' Nineteen'}
-        tens = {2:' Twenty', 3:' Thirty', 4:' Forty', 5:' Fifty', 6:' Sixty', 7:' Seventy', 8:' Eighty', 9:' Ninety'}
-		
-        self.output = ''
-        def wordify(num):
-            if num // 1000000000:
-                wordify(num // 1000000000)
-                self.output += ' Billion'
-                wordify(num % 1000000000)
-            elif num // 1000000:
-                wordify(num // 1000000)
-                self.output += ' Million'
-                wordify(num % 1000000)
-            elif num // 1000:
-                wordify(num // 1000)
-                self.output += ' Thousand'
-                wordify(num % 1000)
-            elif num // 100:
-                wordify(num // 100)
-                self.output += ' Hundred'
-                wordify(num % 100)
-            elif num // 10 - 1 > 0:
-                self.output += tens[num // 10]
-                wordify(num % 10)
-            elif num:
-                self.output += ones[num]
-        wordify(num)
-        return self.output[1:]
+        if num >= 1000000000:
+            res1 = self.convertThreeDigit(num//1000000000) + ' Billion'
+            res2 = self.numberToWords(num%1000000000)
+            return res1 + ' ' + res2 if res2 != 'Zero' else res1
+        elif num >= 1000000:
+            res1 = self.convertThreeDigit(num//1000000) + ' Million'
+            res2= self.numberToWords(num%1000000)
+            return res1 + ' ' + res2 if res2 != 'Zero' else res1
+        elif num >= 1000:
+            res1 = self.convertThreeDigit(num//1000) + ' Thousand'
+            res2 = self.numberToWords(num%1000)
+            return res1 + ' ' + res2 if res2 != 'Zero' else res1
+        else:
+            return self.convertThreeDigit(num)
