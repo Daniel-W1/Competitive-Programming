32a48740f6acdaa4570c6a5bb2dd5ade9c17a58d
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-05-12 12:00:13 +0300

    Time: 68 ms (66.95%), Space: 14.1 MB (59.66%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
@@ -1,24 +1,14 @@
 class Solution:
-    def maxSumTwoNoOverlap(self, nums: List[int], firstLen: int, secondLen: int) -> int:
-        for i in range(1, len(nums)):
-            nums[i] += nums[i-1]
-        
-        # for simplicity
-        f,s = firstLen, secondLen
-        
-        # initialization: maxfirst, maxsecond, maxtotal
-        maxf, maxs, maxt = nums[f-1], nums[s-1], nums[f+s-1]
-        
-        for i in range(f+s, len(nums)):
-            # find the maximum subarray of length f
-            maxf = max(maxf, nums[i-s] - nums[i-s-f])
-            # find the maximum subarray of length s
-            maxs = max(maxs, nums[i-f] - nums[i-f-s])
-            # find the maximum subarray of length f+s
-            maxt = max(maxt, 
-                      max(maxf + nums[i]-nums[i-s], maxs + nums[i]-nums[i-f]))
-        return maxt
-            
+    def maxSumTwoNoOverlap(self, A: List[int], L: int, M: int) -> int:
+        if len(A) < L + M: return 0
+        for i in range(1, len(A)):
+            A[i] += A[i - 1]
+        res, maxL, maxM = A[L + M - 1], A[L - 1], A[M - 1]
+        for i in range(L + M, len(A)):
+            maxL = max(maxL, A[i - M] - A[i - M - L])
+            maxM = max(maxM, A[i - L] - A[i - L - M])
+            res = max(res, maxL + A[i] - A[i - M], maxM + A[i] - A[i - L])
+        return res
             
         
         
