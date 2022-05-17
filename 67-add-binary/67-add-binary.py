d96aea9df15b5acbb0f8f8fb986aad681f34c515
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-05-17 20:14:30 +0300

    Time: 57 ms (26.37%), Space: 14.2 MB (25.19%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,5 +1,6 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+        '''
         first_num = 0
         power = 0
         for i in range(len(a)-1, -1, -1):
@@ -11,10 +12,69 @@ class Solution:
             second_num += (2**(power)*int(b[i]))
             power +=  1
         total = first_num + second_num
-        print(first_num, second_num)
         binary = ""
         while total:
             binary += str((total%2))
             total //= 2
         return binary[::-1] if binary else "0"
+        '''
+        i = len(a)-1
+        j = len(b)-1
+        carry = 0
+        binary = ""
+        while i >= 0 and j >= 0:
+            if a[i] == b[j] == "1" and not carry:
+                binary += "0"
+                carry = 1
+            elif a[i] == b[j] == "1" and carry:
+                binary += "1"
+                carry = 1
+            else:
+                if a[i] != b[j] and not carry:
+                    binary += "1"
+                elif a[i] != b[j] and carry:
+                    binary += "0"
+                    carry = 1
+                elif a[i]==b[j]=="0" and carry:
+                    print(True)
+                    binary += "1"
+                    carry = 0
+                else:
+                    binary += "0"
+            i -= 1
+            j -= 1
+        while i >= 0:
+            if a[i] == "0":
+                if carry:
+                    binary += "1"
+                    carry = 0
+                else:
+                    binary += "0"
+            else:
+                if carry:
+                    binary += "0"
+                    carry = 1
+                else:
+                    binary += "1"
+            i -= 1
+        while j >= 0:
+            print(j)
+            if b[j] == "0":
+                if carry:
+                    binary += "1"
+                    carry = 0
+                else:
+                    binary += "0"
+            else:
+                if carry:
+                    binary += "0"
+                    carry = 1
+                else:
+                    binary += "1"
+            j -= 1
+        while carry:
+            binary += str(carry)
+            carry = 0
+        return binary[::-1]
             
+        
