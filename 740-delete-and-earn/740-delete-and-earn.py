645f03cb6ff13af4443f869c5babb908dce8ac21
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-05-18 16:06:12 +0300

    Time: 172 ms (17.94%), Space: 30.2 MB (6.43%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -2,24 +2,20 @@ class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         points = defaultdict(int)
         max_number = 0
+        # Precompute how many points we gain from taking an element
         for num in nums:
             points[num] += num
             max_number = max(max_number, num)
-        
-        two_back = one_back = 0
-        n = len(points)
-        if max_number < n + n * log(n, 2):
-            one_back = points[1]
-            for num in range(2, max_number + 1):
-                two_back, one_back = one_back, max(one_back, two_back + points[num])
-        else:
-            elements = sorted(points.keys())
-            one_back = points[elements[0]]     
-            for i in range(1, len(elements)):
-                current_element = elements[i]
-                if current_element == elements[i - 1] + 1:
-                    two_back, one_back = one_back, max(one_back, two_back + points[current_element])
-                else:
-                    two_back, one_back = one_back, one_back + points[current_element]
 
-        return one_back
+        @cache
+        def max_points(num):
+            # Check for base cases
+            if num == 0:
+                return 0
+            if num == 1:
+                return points[1]
+            
+            # Apply recurrence relation
+            return max(max_points(num - 1), max_points(num - 2) + points[num])
+        
+        return max_points(max_number)
