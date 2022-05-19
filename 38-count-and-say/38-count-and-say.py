9feedbb88caa20e933dee13f5a2fe6a7d9d2e3e6
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-05-19 09:58:36 +0300

    Time: 673 ms (5.01%), Space: 14.3 MB (8.70%) - LeetHub

--- 38-count-and-say/38-count-and-say.py
+++ 38-count-and-say/38-count-and-say.py
@@ -1,22 +1,46 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        ans = "1"
-        for _ in range(1, n):
-            i = 0
-            newans = ""
-            cnt = 0
-            while i < len(ans):
-                num = ans[i]
-                cnt += 1
-                i += 1
-                while i < len(ans) and ans[i] == num:
+        # ans = "1"
+        # for _ in range(1, n):
+        #     i = 0
+        #     newans = ""
+        #     cnt = 0
+        #     while i < len(ans):
+        #         num = ans[i]
+        #         cnt += 1
+        #         i += 1
+        #         while i < len(ans) and ans[i] == num:
+        #             cnt += 1
+        #             i += 1
+        #         newans += str(cnt) + num
+        #         cnt = 0
+        #     ans = newans
+        # return ans
+        # well the above is an easy iterative solution to this problem, now let's do the
+        # recursive one and we will be good to go
+        def realcount(val, ans):
+            if val == n:
+                return ans
+            ans = "1"
+            for _ in range(1, n):
+                i = 0
+                newans = ""
+                cnt = 0
+                while i < len(ans):
+                    num = ans[i]
                     cnt += 1
                     i += 1
-                newans += str(cnt) + num
-                cnt = 0
-            ans = newans
-        return ans
-                    
+                    while i < len(ans) and ans[i] == num:
+                        cnt += 1
+                        i += 1
+                    newans += str(cnt) + num
+                    cnt = 0
+                ans = newans
+            return realcount(val+1,ans)
+        return realcount(1,"1")
+                
+            
+            
                 
                 
                 
