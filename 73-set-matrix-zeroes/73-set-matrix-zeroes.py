4c78689ca058be9d7784455a70204c687432abda
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-05-26 12:11:27 +0300

    Time: 208 ms (27.71%), Space: 16.1 MB (16.63%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -0,0 +1,19 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        check_map = {}
+        for i,row in enumerate(matrix):
+            for j,val in enumerate(matrix[i]):
+                if val == 0 and (i,j) not in check_map:
+                    for k in range(len(matrix[i])):
+                        if matrix[i][k] != 0:
+                            matrix[i][k] = 0
+                            check_map[i,k] = True
+                    for c,row in enumerate(matrix):
+                        if row[j] != 0:
+                            row[j] = 0
+                            check_map[c,j] = True
+                    
+        
