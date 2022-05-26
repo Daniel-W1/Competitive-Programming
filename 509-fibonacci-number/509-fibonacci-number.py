5f9b2a10f7eee3765188be72e4ae50e22e9f25c1
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-05-26 17:37:04 +0300

    Time: 1201 ms (7.12%), Space: 13.8 MB (55.17%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,13 +1,11 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n <= 1:
+        memo = {}
+        if n < 2:
             return n
-        else:
-            ans = [0]*n
-            ans[0]=ans[1]= 1
-            for i in range(2, len(ans)):
-                ans[i] = ans[i-2]+ans[i-1]
-        return ans[-1]
+        if n not in memo:
+            memo[n] = self.fib(n-1)+self.fib(n-2)
+        return memo[n]
     # the above code is an example of dp and 
     # we can do this problem with naive recursion but that would take a ton of time
     # the time complexity of naive rec and dp is 2^n , O(n) respectively
