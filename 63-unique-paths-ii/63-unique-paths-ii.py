22cdcb25eb0e05dafeec583347ff016ed6d4e75e
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-05-27 10:48:33 +0300

    Time: 50 ms (74.49%), Space: 14.2 MB (20.22%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
+        m = len(grid[0])-1
+        n = len(grid)-1
+        memo = {}
+        print(m, n)
+        def findunique(a, b):
+            if (a,b) not in memo:
+                if a == m and b == n and grid[b][a] != 1:
+                    return 1
+                if a > m or b > n:
+                    return 0
+                if grid[b][a] == 1:
+                    return 0
+                memo[a,b]= findunique(a+1,b) + findunique(a,b+1)
+            return memo[a,b]
+        return findunique(0,0)
+                
