346d6176adae0b6af17077a23778281501418bf6
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-05-27 10:21:38 +0300

    Time: 408 ms (53.43%), Space: 35.9 MB (39.73%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -5,7 +5,7 @@ class Solution:
             if (i,res) not in memo:
                 if i > len(nums)-1:
                     return 1 if res == target else 0
-                memo[i,res]= cntall(i+1, res-nums[i]) + cntall(i+1,res+nums[i])
+                memo[i,res]= cntall(i+1,res+nums[i]) + cntall(i+1,res-nums[i])
             return memo[i,res]
         return cntall(0, 0)
                 
