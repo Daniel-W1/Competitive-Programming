d5b319d19342410e8a508f4c5d09ae7796582fb2
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-05-29 15:06:47 +0300

    Time: 49 ms (99.33%), Space: 14 MB (12.66%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,23 +1,49 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m = len(board)-1
-        n = len(board[0])-1
-        used = {}
-        def findword(a,b,i):
-            if i == len(word):
+        
+        counts = Counter(sum(board, []))
+        
+        if counts[word[0]] > counts[word[-1]]:
+            word = word[::-1]
+            
+        word_counts = collections.Counter(word)
+        
+        for c, count in word_counts.items():
+            if counts[c] < count:
+                return False
+            
+        directions = (
+            (0,1),
+            (0,-1),
+            (1,0),
+            (-1,0)
+        )
+        
+        visiting = [[False]*len(board[0]) for _ in range(len(board))]
+        
+        def in_bounds(i,j):
+            return i < len(board) and j < len(board[0]) and i >= 0 and j >= 0
+        
+        def backtrack(curr, i, j):
+            # print(curr)
+            visiting[i][j] = True
+            if curr == word:
                 return True
-            if a > m or b > n or a < 0 or b < 0 or (a,b) in used:
-                return False
-            if word[i] != board[a][b]:
-                return False
-            used[a,b] = True
-            res =  findword(a+1,b,i+1) or findword(a,b+1,i+1) or findword(a-1,b,i+1) or findword(a,b-1,i+1)
-            del used[a,b]
-            return res
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    check = findword(i,j,0)
-                    if check:
+            
+            target_char = word[len(curr)]
+            for d in directions:
+                di, dj = d[0] + i, d[1] + j
+                if in_bounds(di,dj) and not visiting[di][dj] and board[di][dj] == target_char:
+                    if backtrack(curr + board[di][dj], di, dj):
+                        return True
+                                
+            visiting[i][j] = False
+            return False
+            
+        
+        for y in range(len(board)):
+            for x in range(len(board[y])):
+                if board[y][x] == word[0]:
+                    if backtrack(word[0], y, x):
                         return True
         return False
