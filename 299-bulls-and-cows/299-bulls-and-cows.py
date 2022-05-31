42c0f6e069dce01afb045db4e389ccdd4766c705
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-05-31 15:46:45 +0300

    Time: 162 ms (5.08%), Space: 14 MB (5.22%) - LeetHub

---
+++ 299-bulls-and-cows/299-bulls-and-cows.py
@@ -0,0 +1,26 @@
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        first_map = {}
+        for val in list(secret):
+            if val in first_map:
+                first_map[val][1] += 1
+            else:
+                first_map[val] = [[],1]
+        for i,val in enumerate(list(secret)):
+            first_map[val][0].append(i)
+        cow_count = 0
+        bull_count = 0
+        for i,val in enumerate(list(guess)):
+            if val in first_map:
+                if i in first_map[val][0]:
+                    cow_count += 1
+                    first_map[val][1] -= 1
+        for i,val in enumerate(list(guess)):  
+            if val in first_map:
+                if first_map[val][1] > 0 and i not in first_map[val][0]:
+                    bull_count += 1
+                    first_map[val][1] -= 1
+        return str(cow_count)+"A"+str(bull_count)+"B"
+        
+            
+            
