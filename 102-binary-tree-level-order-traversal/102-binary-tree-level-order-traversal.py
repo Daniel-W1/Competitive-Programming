c389dfa02a08a4f6ade545315ef5527797340dea
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-06-01 18:21:55 +0300

    Time: 71 ms (9.16%), Space: 14.2 MB (51.18%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -6,28 +6,26 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return root
-        q = collections.deque()
-        q.append((root,0))
-        ans = [[q[0][0].val]]
-        other_q = []
-        while q:
-            level = q[0][1]
-            out = []
-            while q and q[0][1] == level:
-                if q[0][0].right or q[0][0].left:
-                    if q[0][0].left:
-                        out.append(q[0][0].left.val)
-                        q.append((q[0][0].left,level+1))
-                        other_q.append((q[0][0].left.val,level+1))
-                    if q[0][0].right:
-                        out.append(q[0][0].right.val)
-                        q.append((q[0][0].right,level+1))
-                        other_q.append((q[0][0].right.val,level+1))
-                q.popleft()
-            ans.append(out)
-        ans.pop()
-        return ans
-                        
         
+        if root is None:
+            return []
+        
+        sol = []
+        ans = []
+
+        sol.append(root)        
+        while len(sol) > 0:
+            
+            ans.append([])
+            s = len(sol)            
+            for i in range(s):
+                curr = sol.pop(0)
+                ans[len(ans)-1].append(curr.val)
+
+                if curr.left:
+                    sol.append(curr.left)
+
+                if curr.right:
+                    sol.append(curr.right)
+                                        
+        return ans
