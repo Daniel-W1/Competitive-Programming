74af4889d08796896a69e94d03c962541949ea7a
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-06-05 15:10:22 +0300

    Time: 53 ms (17.45%), Space: 13.9 MB (54.31%) - LeetHub

--- 1556-thousand-separator/1556-thousand-separator.py
+++ 1556-thousand-separator/1556-thousand-separator.py
@@ -1,15 +1,15 @@
 class Solution:
     def thousandSeparator(self, n: int) -> str:
         num = str(n)
-        new = ""
+        new = []
         i = len(num)-1
         prev = i
         while i >= 0:
-            new += num[i]
+            new.append(num[i])
             if i == prev-2 and i > 0:
-                new += "."
+                new.append(".")
                 prev = i-1
             i -= 1
-        return new[::-1]
+        return "".join(new[::-1])
             
             
