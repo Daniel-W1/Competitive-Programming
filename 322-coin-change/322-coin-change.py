d281dd5f6c9dba5edac485f0e341da518b88d1bc
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-06-07 15:42:05 +0300

    Time: 990 ms (93.83%), Space: 14.2 MB (64.06%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,67 +1,62 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-            if amount == 0: return 0
-            def findmin(coins, amount, memo = {}):
-                if amount in memo: return memo[amount]
-                if amount == 0: return []
-                if amount < 0: return None
-                output = None
-                for coin in coins:
-                    new = amount - coin
-                    res = findmin(coins, new)
-                    if res != None:
-                        res = res + [coin]
-                        if not output or len(res) < len(output):
-                            output = res
-                memo[amount] = output
-                return output
-            minsize = findmin(coins,amount)
-            return len(minsize) if minsize else -1
-        
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-#         dp[0] = 0
-        
-#         for a in range(1, amount+1):
-#             for c in coins:
-#                 if a-c >= 0:
-#                     dp[a] = min(dp[a], 1+dp[a-c])
-#         return dp[amount] if dp[amount] < amount+1 else -1
-    
-    # ugly but fast solution 
-            # cnt, prev = 0, 1 << amount
-            # while prev & 1 == 0:
-            #     curr = prev
+            # if amount == 0: return 0
+            # def findmin(coins, amount, memo = {}):
+            #     if amount in memo: return memo[amount]
+            #     if amount == 0: return []
+            #     if amount < 0: return None
+            #     output = None
             #     for coin in coins:
-            #         curr |= prev >> coin
-            #     if curr == prev:
-            #         return -1
-            #     cnt += 1
-            #     prev = curr
-            # return cnt
+            #         new = amount - coin
+            #         res = findmin(coins, new)
+            #         if res != None:
+            #             res = res + [coin]
+            #             if not output or len(res) < len(output):
+            #                 output = res
+            #     memo[amount] = output
+            #     return output
+            # minsize = findmin(coins,amount)
+            # return len(minsize) if minsize else -1
+        # now let's do this problem using tabulatio or bottom-up
+        table = [-1]*(amount+1)
+        table[0] = 0
+        for i in range(len(table)):
+            if table[i] != -1:
+                for coin in coins:
+                    index = i + coin
+                    if index < len(table):
+                        if table[index] == -1:
+                            table[index] = table[i]+1
+                        else:
+                            new = table[i]+1
+                            if table[index] > new:
+                                table[index] = new
+        return table[amount]
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
