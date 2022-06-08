9dccfad735a378ae71fb3bf38ba4294a7354b07a
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-06-08 15:56:41 +0300

    Time: 178 ms (99.15%), Space: 14.6 MB (18.82%) - LeetHub

--- 654-maximum-binary-tree/654-maximum-binary-tree.py
+++ 654-maximum-binary-tree/654-maximum-binary-tree.py
@@ -6,15 +6,30 @@
 #         self.right = right
 class Solution:
     def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
-        changed_nums = [(val, i) for i,val in enumerate(nums)]
-        def build(changed_nums, l, h):
-            if l < h:
-                the_max = max(changed_nums[l:h])
-                root = TreeNode(the_max[0])
-                root.left = build(changed_nums, l, the_max[1])
-                root.right = build(changed_nums,the_max[1]+1,h)
-                return root
-        return build(changed_nums,0,len(changed_nums))
+        # changed_nums = [(val, i) for i,val in enumerate(nums)]
+        # def build(changed_nums, l, h):
+        #     if l < h:
+        #         the_max = max(changed_nums[l:h])
+        #         root = TreeNode(the_max[0])
+        #         root.left = build(changed_nums, l, the_max[1])
+        #         root.right = build(changed_nums,the_max[1]+1,h)
+        #         return root
+        # return build(changed_nums,0,len(changed_nums))
+        if not nums:
+                return None
+        stk = []
+        last = None
+        for num in nums:
+            while stk and stk[-1].val < num:
+                last = stk.pop()
+            node = TreeNode(num)
+            if stk:
+                stk[-1].right = node 
+            if last:
+                node.left = last
+            stk.append(node)
+            last = None
+        return stk[0]
         
             
         
