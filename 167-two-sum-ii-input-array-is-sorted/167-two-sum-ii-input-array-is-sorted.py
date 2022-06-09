0872bc76eb2b6aa8cbc58337cbf5f2d48b14fcad
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-06-09 09:23:23 +0300

    Time: 212 ms (31.45%), Space: 14.9 MB (89.05%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,8 +1,9 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         all_map = {}
-        for i,val in enumerate(numbers):
+        for i,val in enumerate(nums):
             all_map[val] = i+1
-        for i,val in enumerate(numbers):
-            if target-val in all_map and all_map[target-val] !=  i+1:
-                return [i+1,all_map[target-val]]
+        for i,val in enumerate(nums):
+            if target-val in all_map and all_map[target-val] != i+1:
+                return [i+1, all_map[target-val]]
+        
