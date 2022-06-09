8a03cdca9a9eb39a78ec2264700248dfe67c995b
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-06-09 09:42:54 +0300

    Time: 289 ms (7.60%), Space: 15.1 MB (11.40%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,9 +1,31 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        all_map = {}
+#         all_map = {}
+#         for i,val in enumerate(nums):
+#             all_map[val] = i+1
+#         for i,val in enumerate(nums):
+#             if target-val in all_map and all_map[target-val] != i+1:
+#                 return [i+1, all_map[target-val]]
+        def binarysearch(l,h,targ):
+            while l <= h:
+                mid = (l+h)//2
+                if nums[mid] == targ:
+                    return mid
+                elif nums[mid] > targ:
+                    h = mid -1
+                else:
+                    l = mid + 1
+            return -1
         for i,val in enumerate(nums):
-            all_map[val] = i+1
-        for i,val in enumerate(nums):
-            if target-val in all_map and all_map[target-val] != i+1:
-                return [i+1, all_map[target-val]]
+            new_target = target - val
+            nums[i] = val-0.5
+            if new_target >= val:
+                check = binarysearch(i, len(nums)-1,new_target)
+            else:
+                check = binarysearch(0,i,new_target)
+            if check >= 0:
+                return [i+1,check+1]
+            nums[i] = val
         
+                
+                
