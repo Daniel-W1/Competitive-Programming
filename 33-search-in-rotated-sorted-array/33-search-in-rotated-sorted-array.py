18ec9a5f8bbe77b4f2194d0bf812197d7334d2bf
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-06-11 14:59:37 +0300

    Time: 40 ms (94.35%), Space: 14.3 MB (55.92%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -2,13 +2,13 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l,h = 0,len(nums)-1
         # this part is just trying to find the maximum in the array
-        
         while l < h:
             mid = (l+h)//2
             if nums[l] < nums[mid]:
                 l = mid
             else:
                 h = mid
+                
         h = len(nums)-1
         def binarysearch(l,h):
             while l <= h:
@@ -20,6 +20,7 @@ class Solution:
                 else:
                     l = mid+1
             return -1
+        
         res1 = binarysearch(0,l)
         res2 = binarysearch(l+1,h)
         
