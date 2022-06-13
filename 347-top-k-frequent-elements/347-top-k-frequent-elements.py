44d50a0aff3787c829bf049cd92528b13317300b
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-06-13 21:53:15 +0300

    Update 347-top-k-frequent-elements.py

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,25 +1,20 @@
-from collections import Counter
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+        arr = [[v,k] for k, v in count.items()]
+        min_heap = arr[:k]
+        heapq.heapify(min_heap)
+        for it in arr[k:]:
+            heapq.heappushpop(min_heap, it)
+        return [p[1] for p in heapq.nsmallest(k, min_heap)]
         
-        count= Counter(nums)
-        collector= []
-
-        for ke, v in count.items():
-            collector.append((ke,v))
-        collector = sorted(collector, key= lambda val: val[1], reverse = True)
-
-        ans=[]
-
-        if k ==1:
-            return [collector[0][0]]
-
-        for i in range(k):
-            ans.append(collector[i][0])
-
-        return ans
-
+        
+            
+            
+        
+                    
+            
+        
 
 
         
