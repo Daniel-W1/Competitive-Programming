a1c938867da61cc2b960c3b7931b894a823e1343
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-06-17 10:35:33 +0300

    Time: 44 ms (72.04%), Space: 13.8 MB (73.35%) - LeetHub

---
+++ 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def distributeCoins(self, root: Optional[TreeNode]) -> int:
+        self.ans = 0
+        def dfs(root):
+            if not root: return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            
+            self.ans += abs(left)+abs(right)
+            return root.val+left+right-1
+        dfs(root)
+        return self.ans
