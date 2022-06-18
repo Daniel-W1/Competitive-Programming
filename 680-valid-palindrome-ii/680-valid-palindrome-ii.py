d2d6e4eaa1ff2aa9747dde4240db4d7a2d5cf42d
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-06-18 22:43:11 +0300

    Time: 355 ms (12.30%), Space: 14.5 MB (45.49%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,20 +1,19 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def ispali(l,h,word):
-            l,h = 0,len(word)-1
+        def ispali(l,h):
             while l < h:
-                if word[l] != word[h]:
+                if s[l] != s[h]:
                     return False
                 l += 1
                 h -= 1
             return True
         l,h = 0,len(s)-1
+        
         while l < h:
             if s[l] != s[h]:
-                word1 = s[:h] + s[h+1:]
-                word2 = s[:l] + s[l+1:]
-                check1 = ispali(l,h,word1)
-                check2 = ispali(l,h,word2)
+                check1 = ispali(l+1,h)
+                check2 = ispali(l,h-1)
+                
                 return check1 or check2
             l += 1
             h -= 1
