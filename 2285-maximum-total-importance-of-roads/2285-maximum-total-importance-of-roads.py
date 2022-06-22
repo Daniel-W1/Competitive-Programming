329658a6cf22b7fdd4fad6493875ce721604ba55
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-06-22 15:22:40 +0300

    Time: 2906 ms (24.90%), Space: 39.6 MB (68.41%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.py
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.py
@@ -1,22 +1,15 @@
 class Solution:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
-        def buildgraph(roads):
-            graph = {}
-            for road in roads:
-                if road[0] not in graph:
-                    graph[road[0]] = []
-                if road[1] not in graph:
-                    graph[road[1]] = []
-                    
-                graph[road[0]].append(road[1])
-                graph[road[1]].append(road[0])
-            return graph
-        graph = buildgraph(roads)
-        helper = [-len(val) for val in graph.values()]
-        heapq.heapify(helper)
-        length = len(helper)
-        answer,max_value = 0,n
-        for _ in range(length):
-            answer += -(max_value*(heapq.heappop(helper)))
-            max_value -= 1
-        return answer
+        cnt = [0 for _ in range(n)]
+        for a, b in roads:
+            cnt[a] += 1
+            cnt[b] += 1
+        cnt.sort(reverse = True)
+        ans = 0
+        w = n
+        for c in cnt:
+            if c == 0:
+                break
+            ans += c*w
+            w -= 1
+        return ans
