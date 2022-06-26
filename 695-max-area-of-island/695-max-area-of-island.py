e9216607a450ccaee5f5988014132c8ca9d125be
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-06-26 22:04:12 +0300

    Time: 187 ms (62.73%), Space: 16.9 MB (40.39%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        bigset = set()
+
         def dfs(i,j,visited):
             if (i,j) in visited:
                 return
@@ -10,18 +10,18 @@ class Solution:
                 return
             self.cnt += 1
             visited.add((i,j))
-            bigset.add((i,j))
             dfs(i+1,j,visited)
             dfs(i-1,j,visited)
             dfs(i,j+1,visited)
             dfs(i,j-1,visited)
         
         ans = 0
+        visited = set()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    if (i,j) not in bigset:
-                        self.cnt,visited = 0, set()
+                    if (i,j) not in visited:
+                        self.cnt = 0
                         dfs(i,j,visited)
                         ans = max(ans,self.cnt)
         return ans
