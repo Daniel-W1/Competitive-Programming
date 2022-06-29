d9754a2832d7e1b686c74e3d96d1ef0fb3da7757
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-06-29 16:53:00 +0300

    Time: 110 ms (82.52%), Space: 14.5 MB (31.23%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -1,13 +1,7 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        people.sort(key = lambda val:val[0])
-        ans = [0]*len(people)
-        for h,k in people:
-            cnt = k
-            pos = 0
-            while cnt > 0 or isinstance(ans[pos],list):
-                if ans[pos] == 0 or ans[pos][0] >= h:
-                    cnt -= 1
-                pos += 1
-            ans[pos] = [h,k]
-        return ans
+        people.sort(key = lambda pair : (-pair[0], pair[1]))
+        queue = []
+        for p in people:
+            queue.insert(p[1], p)
+        return queue
