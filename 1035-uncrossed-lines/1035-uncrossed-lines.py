24fd7f43e36a7133f60f745630f983f3cb859ce8
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-06-30 13:56:59 +0300

    Time: 341 ms (44.53%), Space: 14.2 MB (63.45%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.py
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -1,14 +1,22 @@
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
         # this is the same us finding the longest common number subsequence
-        n,m = len(nums1)-1, len(nums2)-1
-        def LCNS(i,j,memo = {}):
-            if (i,j) in memo: return memo[i,j]
-            if i > n or j > m:
-                return 0
-            if nums1[i] == nums2[j]:
-                memo[i,j] = 1 + LCNS(i+1,j+1)
-                return memo[i,j]
-            memo[i,j] = max(LCNS(i,j+1),LCNS(i+1,j))
-            return memo[i,j]
-        return LCNS(0,0)
+        # n,m = len(nums1)-1, len(nums2)-1
+        # def LCNS(i,j,memo = {}):
+        #     if (i,j) in memo: return memo[i,j]
+        #     if i > n or j > m:
+        #         return 0
+        #     if nums1[i] == nums2[j]:
+        #         memo[i,j] = 1 + LCNS(i+1,j+1)
+        #         return memo[i,j]
+        #     memo[i,j] = max(LCNS(i,j+1),LCNS(i+1,j))
+        #     return memo[i,j]
+        # return LCNS(0,0)
+        dp = [[0 for _ in range(len(nums1)+1)] for _ in range(len(nums2)+1)]
+        for i in range(len(nums2)-1,-1,-1):
+            for j in range(len(nums1)-1,-1,-1):
+                if nums1[j] == nums2[i]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j],dp[i][j+1])
+        return dp[0][0]
