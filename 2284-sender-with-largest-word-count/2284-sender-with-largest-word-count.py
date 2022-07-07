cd7a522068874406944c4415e382dede143f11d8
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-07-07 22:31:09 +0300

    Time: 543 ms (30.92%), Space: 22.4 MB (4.43%) - LeetHub

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.py
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.py
@@ -6,14 +6,13 @@ class Solution:
         for word_cnt, index in senders_map:
             senders_word_cnt[senders[index]] += word_cnt
         dict_items = list(senders_word_cnt.items())
-        dict_items.sort(key = lambda value : value[1], reverse = True)
+        max_cnt = max(senders_word_cnt.values())
         
-        max_cnt = dict_items[0][1]
-        collected = []
-        for i,item in enumerate(dict_items):
-            if item[1] == max_cnt:
-                collected.append(item)
-            else:
-                break
-        collected.sort(reverse = True)
-        return collected[0][0]
+        max_name = None
+        for key, value in senders_word_cnt.items():
+            if senders_word_cnt[key] == max_cnt:
+                if not max_name:
+                    max_name = key
+                else:
+                    max_name = max(max_name, key)
+        return max_name
