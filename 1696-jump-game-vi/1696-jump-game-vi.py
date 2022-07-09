4e7031f31bc7e4a3bb533853f5351bca07813a0a
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-07-09 18:52:21 +0300

    Time: 2364 ms (9.60%), Space: 27.6 MB (66.51%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -1,14 +1,23 @@
 class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
-        deq, n = deque([0]), len(nums)
-        for i in range(1, n):
-            while deq and deq[0] < i - k: deq.popleft()
-            nums[i] += nums[deq[0]]   
-            while deq and nums[i] >= nums[deq[-1]]: deq.pop()
-            deq.append(i)
+        # so let's do this and feel great about our self
+        # [1,-1,-2,4,-7,3] k = 2
+        # total = nums[0]
+        # so i don't understand why this problem is a dp problem
+        # i just need to choose the maximum of what i see and return 
+        # the maximumscore
+        
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        d = deque([(nums[0],0)])
+        for i in range(1, len(nums)):
+            dp[i] = nums[i] + d[0][0]
             
-        return nums[-1]
-
+            while d and d[-1][0] < dp[i]:   # sliding window maximum variation
+                d.pop()                     # sliding window maximum variation
+            d.append((dp[i],i))             # sliding window maximum variation
             
-                    
-            
+            if i-k == d[0][1]:              # sliding window maximum variation
+                d.popleft()                 # sliding window maximum variation
+                
+        return dp[-1]
