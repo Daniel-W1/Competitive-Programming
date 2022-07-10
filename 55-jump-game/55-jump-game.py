b11e5e5ebc60e4e93ee165b47a503264616573e8
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-07-10 09:40:17 +0300

    Time: 895 ms (28.22%), Space: 15.4 MB (18.15%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -8,7 +8,21 @@ class Solution:
                 
             
 
+    # so am trying to solve this thing with a dp 
+    # and the first thing that came to mind is it's actually very similar
+    # with the coin change problem with a little twist.
+    # [2,3,1,1,4]
+    # [3,2,1,0,4]
+        fartherst_reach = nums[0]
+        for i in range(1, len(nums)):
+            if i > farthest_reach:
+                return False
+            farthest_reach = max(farthest_reach, i+nums[i])
+        return True
 
+        
+        
+    
 
 
 
