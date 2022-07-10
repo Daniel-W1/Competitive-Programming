60d5587f5692fb4b53d4140c37fe500736317604
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-07-10 07:53:35 +0300

    Time: 134 ms (6.60%), Space: 16.2 MB (10.95%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,23 +1,21 @@
 class Solution:
     def minCostClimbingStairs(self, nums: List[int]) -> int:
-#             if not cost:
-#                 return 0
-#             dp = [0] * len(cost)
-#             dp[0] = cost[0]
-#             if len(cost) >= 2:
-#                 dp[1] = cost[1]
-#             for i in range(2, len(cost)):
-#                 dp[i] = cost[i] + min(dp[i-1], dp[i-2])
-
-#             return min(dp[-1], dp[-2])
-        memo = {}
-        memo[len(nums)-1], memo[len(nums)-2] = nums[len(nums)-1], nums[len(nums)-2]
-        def smart_min(i):
-            if i not in memo:
-                memo[i] = nums[i] + min(smart_min(i+1), smart_min(i+2))
-            return memo[i]
-        return min(smart_min(0), smart_min(1))
-
-                
-            
+        # so what are the conditons for this problem
+        # so reaching the last of the prev last index is considered as a 
+        # so the best way to do this is just to call the function
+        # twice for both the 0th and the 1st index
+        # so the code below works but the quesion is how do we memoise it
         
+        # let's sketch our decision tree on paper first
+        # so from sketching it out we can obviously see that 
+        # the index we are at can used as a key to memoise the code
+        memo = {}
+        def minCost(i):
+            if i in memo: return memo[i]
+            if i >= len(nums)-2:
+                return nums[i]
+            memo[i] = min(minCost(i+1)+nums[i], minCost(i+2)+nums[i])
+            return memo[i]
+        return min(minCost(0), minCost(1))
+    
+    
