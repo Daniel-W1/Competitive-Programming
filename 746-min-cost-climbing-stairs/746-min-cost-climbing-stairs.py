7bbb9eb889b71ea23497f3ceb26f987ff92601c7
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-07-10 08:07:11 +0300

    Time: 102 ms (35.15%), Space: 14 MB (44.40%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -9,13 +9,37 @@ class Solution:
         # let's sketch our decision tree on paper first
         # so from sketching it out we can obviously see that 
         # the index we are at can used as a key to memoise the code
-        memo = {}
-        def minCost(i):
-            if i in memo: return memo[i]
-            if i >= len(nums)-2:
-                return nums[i]
-            memo[i] = min(minCost(i+1)+nums[i], minCost(i+2)+nums[i])
-            return memo[i]
-        return min(minCost(0), minCost(1))
+        # memo = {}
+        # def minCost(i):
+        #     if i in memo: return memo[i]
+        #     if i >= len(nums)-2:
+        #         return nums[i]
+        #     memo[i] = min(minCost(i+1)+nums[i], minCost(i+2)+nums[i])
+        #     return memo[i]
+        # return min(minCost(0), minCost(1))
+    # so the next problem is can we do this very easy problem by using 
+    # a bottom up approach ??
+    
+    # so i got an idea and let's queickly implement it and study math
+        dp = [float('inf')]*len(nums)
+        dp[0], dp[1] = nums[0], nums[1]
+
+        for i in range(2, len(nums)):
+            dp[i] = nums[i] + min(dp[i-1], dp[i-2])
+        return min(dp[-1], dp[-2])
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
