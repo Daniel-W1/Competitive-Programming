23add01048db0fc9d9b07e5f2bbd7cb4ef5dcc0c
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-07-24 09:34:51 +0300

    Time: 1400 ms (14.35%), Space: 36.5 MB (29.80%) - LeetHub

--- 148-sort-list/148-sort-list.py
+++ 148-sort-list/148-sort-list.py
@@ -4,42 +4,24 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def length(self, head):
-        len = 0
-        while head:
-            head = head.next
-            len += 1
-        return len
-    def merge(self, head1, head2):
-        dummy = tail = ListNode()
-        while head1 and head2:
-            if head1.val < head2.val:
-                tail.next = head1
-                head1,tail = head1.next, tail.next
+    def merge(self, h1, h2):
+        dummy = tail = ListNode(None)
+        while h1 and h2:
+            if h1.val < h2.val:
+                tail.next, tail, h1 = h1, h1, h1.next
             else:
-                tail.next = head2
-                head2, tail = head2.next, tail.next
-        tail.next = head1 or head2
+                tail.next, tail, h2 = h2, h2, h2.next
+    
+        tail.next = h1 or h2
         return dummy.next
-    def split(self, head):
-        leng = self.length(head)
-        mid = leng//2
-        index = 0
-        
-        current = head
-        while index < mid-1:
-            current = current.next
-            index += 1
-        right = current.next
-        current.next = None
-        return head, right
-            
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if self.length(head) < 2:
+    
+    def sortList(self, head):
+        if not head or not head.next:
             return head
-        left, right = self.split(head)
-        left = self.sortList(left)
-        right = self.sortList(right)
-        
-        res = self.merge(left,right)
-        return res
+    
+        pre, slow, fast = None, head, head
+        while fast and fast.next:
+            pre, slow, fast = slow, slow.next, fast.next.next
+        pre.next = None
+
+        return self.merge(*map(self.sortList, (head, slow)))
