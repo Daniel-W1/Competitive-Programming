41f7747b480e8b91e1a76cfd31b58c35d7c032e5
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-07-24 10:16:51 +0300

    Time: 123 ms (27.54%), Space: 17.9 MB (13.46%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.py
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.py
@@ -5,15 +5,28 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        dummy = tail = ListNode(0)
-        cur = head
-        while cur:
-            if cur.val != val: 
-                tail.next = ListNode(cur.val)
-                tail = tail.next
-            cur = cur.next
-        return dummy.next
+        # dummy = tail = ListNode(0)
+        # cur = head
+        # while cur:
+        #     if cur.val != val: 
+        #         tail.next = ListNode(cur.val)
+        #         tail = tail.next
+        #     cur = cur.next
+        # return dummy.next
         # this works but this is not a constant space solution or it creates new listnode 
         # so we need to come up with the constant space solution
         # time O(n)
         # space O(n)
+        # i need to come up with the O(1) not done here
+        dummy = ListNode(0)
+        res = dummy
+        
+        dummy.next = head
+        
+        while dummy.next:
+            if dummy.next.val == val:
+                dummy.next = dummy.next.next
+            else:
+                dummy = dummy.next
+        
+        return res.next
