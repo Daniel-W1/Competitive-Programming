4a5ab12f6baaba4e2bc3374c00a20a0b8ad09dc3
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-07-24 09:27:11 +0300

    Time: 1695 ms (7.12%), Space: 36.3 MB (88.68%) - LeetHub

--- 148-sort-list/148-sort-list.py
+++ 148-sort-list/148-sort-list.py
@@ -4,22 +4,42 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def length(self, head):
+        len = 0
+        while head:
+            head = head.next
+            len += 1
+        return len
+    def merge(self, head1, head2):
+        dummy = tail = ListNode()
+        while head1 and head2:
+            if head1.val < head2.val:
+                tail.next = head1
+                head1,tail = head1.next, tail.next
+            else:
+                tail.next = head2
+                head2, tail = head2.next, tail.next
+        tail.next = head1 or head2
+        return dummy.next
+    def split(self, head):
+        leng = self.length(head)
+        mid = leng//2
+        index = 0
+        
         current = head
-        arrlist = []
-        while current:
-            arrlist.append(current.val)
+        while index < mid-1:
             current = current.next
+            index += 1
+        right = current.next
+        current.next = None
+        return head, right
             
-        arrlist = sorted(arrlist)
-        if not arrlist:
-            return None
-        new = ans = ListNode(arrlist[0]) 
-        for val in arrlist[1:]:
-            ans.next = ListNode(val)
-            ans = ans.next
-        return new
-    
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if self.length(head) < 2:
+            return head
+        left, right = self.split(head)
+        left = self.sortList(left)
+        right = self.sortList(right)
         
-            
-        
+        res = self.merge(left,right)
+        return res
