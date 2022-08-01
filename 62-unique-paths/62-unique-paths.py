3082a834a5d465953e7836ae468bf0b2337da6f7
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-08-01 18:35:28 +0300

    Time: 46 ms (60.39%), Space: 13.9 MB (32.97%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,14 +1,3 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def move(i,j,memo = {}): 
-            if (i,j) in memo: return memo[i,j]
-            if i > m-1 or j > n-1:
-                return 0
-            if i == m-1 and j == n-1:
-                return 1
-            memo[i,j] = move(i+1, j) + move(i,j+1)
-            return memo[i,j]
-        return move(0,0)
-    
-    # but this thing need to be memoised 
-    # time comp and space comp
+        return round(factorial(m+n-2)/(factorial(m-1)*factorial(n-1)))
