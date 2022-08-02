17760c19e71b2e53bebac18f16c76889cbfdd446
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-08-02 21:53:47 +0300

    Create 347-top-k-frequent-elements.py

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,27 +1,35 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]: 
+        
+        # bucket sort O(n) time, O(n) space
+#         count = {}
+#         freq = [[] for i in range(len(nums) + 1)]
+
+#         for n in nums:
+#             count[n] = 1 + count.get(n, 0)
+#         for n, c in count.items():
+#             freq[c].append(n)
+
+#         res = []
+#         for i in range(len(freq) - 1, 0, -1):
+#             for n in freq[i]:
+#                 res.append(n)
+#             if len(res) == k:
+#                 return res
+
+        if k == len(nums): return nums
+    
         count = Counter(nums)
-        arr = [[v,k] for k, v in count.items()]
-        min_heap = arr[:k]
-        heapq.heapify(min_heap)
-        for it in arr[k:]:
-            heapq.heappushpop(min_heap, it)
-        return [p[1] for p in heapq.nsmallest(k, min_heap)]
+        freqs = [(val, key) for key, val in count.items()]
         
+        minHeap = []
         
-            
-            
+        for pair in freqs:
+            if len(minHeap) < k:
+                heapq.heappush(minHeap, pair)
+            else:
+                heapq.heappushpop(minHeap, pair)
         
-                    
-            
-        
-
-
-        
-        
-        
-
-        
-            
-        
-        
+        return [pair[1] for pair in minHeap]
+    
+    # time comp O()
