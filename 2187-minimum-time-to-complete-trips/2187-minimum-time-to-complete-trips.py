a33d2023421e75cdd4e9b8a7a41759431f353e53
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-08-05 22:49:50 +0300

    Time: 4441 ms (31.94%), Space: 28.5 MB (49.03%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.py
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.py
@@ -1,14 +1,15 @@
 class Solution:
     def minimumTime(self, time: List[int], totalTrips: int) -> int:
-        l = 0
-        tail = totalTrips*min(time)
-        while l < tail:
-            mid = (l+tail)//2
-            cnt = 0
-            for val in time:
-                cnt += (mid//val)
-            if cnt >= totalTrips:
-                tail = mid
+        left, right = 0, 10**15
+        while left <= right:
+            mid = (left + right)//2
+            trips = 0
+            for t in time:
+                trips += mid//t
+            if trips >= totalTrips:
+                right = mid - 1
             else:
-                l = mid + 1
-        return l
+                left = mid + 1
+                
+        return right + 1
+                
