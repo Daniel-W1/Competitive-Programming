dd96fad49c9824a07a254e0997370483ce1891f8
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-08-07 19:42:37 +0300

    Create 911-online-election.py

--- 911-online-election/911-online-election.py
+++ 911-online-election/911-online-election.py
@@ -1,20 +1,33 @@
-class TopVotedCandidate(object):
-    def __init__(self, persons, times):
-        self.A = []
-        count = collections.Counter()
-        leader, m = None, 0  # leader, num votes for leader
+class TopVotedCandidate:
 
-        for p, t in itertools.izip(persons, times):
-            count[p] += 1
-            c = count[p]
-            if c >= m:
-                if p != leader:  # lead change
-                    leader = p
-                    self.A.append((t, leader))
+    def __init__(self, persons: List[int], times: List[int]):
+        self.persons = persons
+        self.times = times
+        self.precomp = []
+        count = {}
+        the_max = (0, 0)
+        for num in self.persons:
+            count[num] = count.get(num, 0) + 1
+            if count[num] >= the_max[1]:
+                the_max = (num, count[num])
+            self.precomp.append(the_max[0])
 
-                if c > m:
-                    m = c
+    def q(self, t: int) -> int:
+        index = self.binarySearch(self.times, t)
+        return self.precomp[index]                
 
-    def q(self, t):
-        i = bisect.bisect(self.A, (t, float('inf')), 1)
-        return self.A[i-1][1]
+    def binarySearch(self, nums, target):
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = (left + right)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid -1 
+            else:
+                left = mid + 1
+        return right
+
+# Your TopVotedCandidate object will be instantiated and called as such:
+# obj = TopVotedCandidate(persons, times)
+# param_1 = obj.q(t)
