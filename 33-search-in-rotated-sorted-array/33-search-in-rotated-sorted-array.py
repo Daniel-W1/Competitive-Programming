f23cb89858953912b04f68ad58cac1739ebcb452
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-08-08 12:17:31 +0300

    Time: 49 ms (83.30%), Space: 14.3 MB (18.83%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,29 +1,25 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l,h = 0,len(nums)-1
-        # this part is just trying to find the maximum in the array
-        while l < h:
-            mid = (l+h)//2
-            if nums[l] < nums[mid]:
-                l = mid
+        # we need to find the minimum number that is less that cur
+        cur = nums[0]
+        left, right = 0, len(nums)-1
+        
+        while left <= right:
+            mid = (left + right)//2
+            if nums[mid] >= cur:
+                left = mid + 1
             else:
-                h = mid
-                
-        h = len(nums)-1
-        def binarysearch(l,h):
-            while l <= h:
-                mid = (l+h)//2
+                right = mid - 1
+        minimum = right + 1
+        
+        def binaryS(left, right):
+            while left <= right:
+                mid = (left + right)//2
                 if nums[mid] == target:
                     return mid
-                elif nums[mid] > target:
-                    h = mid-1
+                if nums[mid] > target:
+                    right = mid - 1
                 else:
-                    l = mid+1
+                    left = mid + 1
             return -1
-        
-        res1 = binarysearch(0,l)
-        res2 = binarysearch(l+1,h)
-        
-        return max(res1,res2)
-    
-        
+        return max(binaryS(0, minimum-1), binaryS(minimum, len(nums)-1))
