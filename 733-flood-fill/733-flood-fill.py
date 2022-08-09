34fd0c9eac24028b6b5000a1f339896a6dbafd1c
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-08-09 10:46:34 +0300

    Time: 147 ms (18.89%), Space: 14.3 MB (14.38%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,19 +1,20 @@
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        m = len(image)-1
-        n = len(image[0])-1
-        check = {}
-        def dfs(a,b,prev):
-            if a > m or b > n or a < 0 or b < 0 or (a,b) in check:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        m, n = len(image)-1, len(image[0])-1
+        iBound = lambda i: i >=0 and i <= m
+        jBound = lambda j: j >= 0 and j <= n
+        start = image[sr][sc]
+        def dfs(i,j, visited):
+            if not iBound(i) or not jBound(j) or (i,j) in visited or image[i][j] != start:
                 return 
-            if image[a][b] != prev:
-                return
-            check[a,b] = True
-            prev = image[a][b]
-            image[a][b] = newColor
-            dfs(a+1,b,prev)
-            dfs(a-1,b,prev)
-            dfs(a,b+1,prev)
-            dfs(a,b-1,prev)
-        dfs(sr,sc,image[sr][sc])
+
+            image[i][j] = color
+            visited.add((i,j))
+            dfs(i+1, j, visited)
+            dfs(i, j+1, visited)
+            dfs(i-1, j, visited)
+            dfs(i, j-1, visited)
+        dfs(sr, sc, set())
         return image
+            
+            
