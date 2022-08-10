67a3a257d063a7b069061f96658a49726011d9f4
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-08-10 10:22:37 +0300

    Time: 251 ms (32.03%), Space: 17.7 MB (24.39%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,27 +1,27 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-
-        def dfs(i,j,visited):
-            if (i,j) in visited:
+        answer = 0
+        ibound = lambda i : i >= 0 and i < len(grid)
+        jbound = lambda j : j >= 0 and j < len(grid[0])
+        def dfs(sr, sc, visited):
+            nonlocal cnt
+            if not ibound(sr) or not jbound(sc) or (sr,sc) in visited:
+                return 
+            if grid[sr][sc] == 0:
                 return
-            if i < 0 or i > len(grid)-1 or j < 0 or j > len(grid[0])-1:
-                return
-            if grid[i][j] == 0:
-                return
-            self.cnt += 1
-            visited.add((i,j))
-            dfs(i+1,j,visited)
-            dfs(i-1,j,visited)
-            dfs(i,j+1,visited)
-            dfs(i,j-1,visited)
-        
-        ans = 0
+            cnt += 1
+            visited.add((sr, sc))
+            dfs(sr+1, sc, visited)
+            dfs(sr, sc + 1, visited)
+            dfs(sr - 1, sc, visited)
+            dfs(sr, sc - 1, visited)
         visited = set()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    if (i,j) not in visited:
-                        self.cnt = 0
-                        dfs(i,j,visited)
-                        ans = max(ans,self.cnt)
-        return ans
+                if (i,j) not in visited and grid[i][j] == 1:
+                    cnt = 0 
+                    dfs(i,j, visited)
+                    answer = max(answer, cnt)
+        return answer
+            
+            
