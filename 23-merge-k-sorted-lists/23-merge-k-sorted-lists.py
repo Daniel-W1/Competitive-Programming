d55dd1fab1c2558e759dfea90a7ae569ec005206
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-08-14 18:32:38 +0300

    Time: 144 ms (70.56%), Space: 17.9 MB (52.19%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -1,29 +1,20 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-from collections import deque
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
- 
-        while len(lists) > 1:
-            new = []
-            for i in range(0, len(lists), 2):
-                dummy =tail = ListNode()
-                if i + 1 < len(lists) : first, second = lists[i], lists[i+1]
-                else:
-                    new.append(lists[i])
-                    break
-                    
-                while first and second:
-                    if first.val < second.val:
-                        tail.next, first = first, first.next
-                    else:
-                        tail.next, second = second, second.next
-                    tail = tail.next
-
-                tail.next = first or second
-                new.append(dummy.next)
-            lists = new
-        return lists[0] if len(lists) > 0 else None
+        minHeap = []
+        for i, list in enumerate(lists):
+            if list:
+                heapq.heappush(minHeap, (list.val, i, list))
+            
+        dummy = tail = ListNode()
+        while minHeap:
+            _, __, node = heapq.heappop(minHeap)
+            next = node.next
+            node.next = None
+            tail.next = node
+            tail = tail.next
+            if next:
+                heapq.heappush(minHeap, (next.val, __, next))
+            
+        return dummy.next
+        
+            
