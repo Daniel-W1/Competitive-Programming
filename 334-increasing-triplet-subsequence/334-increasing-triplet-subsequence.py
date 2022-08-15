bc75474dcf29b16e0bfbb52904d739649e78de02
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-08-15 12:38:35 +0300

    Time: 990 ms (37.42%), Space: 24.5 MB (77.84%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
@@ -1,40 +1,13 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        # stack monoStack increasing
-        # 1, 2, 3
-        # 1, 5, 0
-        # [2,1,5,0,6]
-        # 0, 6
-        # 1,5,6
-        
-        #[2,1,5,0,4,6]
-        # 2, 5, 6
-        # n^3
-        # for every n we are looking another partners in the whole array
-        # n^2
-        # [2,1,5,0,4,6]
-        # finding an element that has next greater and previous smaller
-        
-        
-        # step1 check if their is a next greater element
-        check1 = [False]*len(nums)
-        stack = []
-        for idx,number in enumerate(nums):
-            while stack and number > stack[-1][1]:
-                index, value = stack.pop()
-                check1[index] = True
-            stack.append((idx, number))
-
-        check2 = [False]*len(nums)
-        stack = []
-        for idx in range(len(nums)-1,-1,-1):
-            number = nums[idx]
-            while stack and number < stack[-1][1]:
-                index, value = stack.pop()
-                check2[index] = True
-            stack.append((idx, number))
-            
-        for idx in range(len(check1)):
-            if check1[idx] and check2[idx]:
+        first_num = float("inf")
+        second_num = float("inf")
+        for n in nums:
+            if n <= first_num:
+                first_num = n
+            elif n <= second_num:
+                second_num = n
+            else:
                 return True
         return False
+    
