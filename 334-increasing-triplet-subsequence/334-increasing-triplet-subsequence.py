2f54fc9bc59b94dd64c4486f279bd46a9b2a62c2
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-08-15 11:54:03 +0300

    Time: 1206 ms (16.36%), Space: 52.4 MB (5.43%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
@@ -1,20 +1,40 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        if len(nums) < 3:
-            return False
-        premins = [0]*len(nums)
-        postmaxs = [0]*len(nums)
-        prevmin = nums[0]
-        prevmax = nums[-1]
+        # stack monoStack increasing
+        # 1, 2, 3
+        # 1, 5, 0
+        # [2,1,5,0,6]
+        # 0, 6
+        # 1,5,6
         
-        for i in range(len(nums)):
-            premins[i] = min(prevmin,nums[i])
-            prevmin = premins[i]
-        for i in range(len(nums)-1,-1,-1):
-            postmaxs[i] = max(prevmax,nums[i])
-            prevmax = postmaxs[i]
-        for i in range(1,len(nums)-1):
-            if premins[i-1] <nums[i] < postmaxs[i+1]:
+        #[2,1,5,0,4,6]
+        # 2, 5, 6
+        # n^3
+        # for every n we are looking another partners in the whole array
+        # n^2
+        # [2,1,5,0,4,6]
+        # finding an element that has next greater and previous smaller
+        
+        
+        # step1 check if their is a next greater element
+        check1 = [False]*len(nums)
+        stack = []
+        for idx,number in enumerate(nums):
+            while stack and number > stack[-1][1]:
+                index, value = stack.pop()
+                check1[index] = True
+            stack.append((idx, number))
+
+        check2 = [False]*len(nums)
+        stack = []
+        for idx in range(len(nums)-1,-1,-1):
+            number = nums[idx]
+            while stack and number < stack[-1][1]:
+                index, value = stack.pop()
+                check2[index] = True
+            stack.append((idx, number))
+            
+        for idx in range(len(check1)):
+            if check1[idx] and check2[idx]:
                 return True
         return False
-            
