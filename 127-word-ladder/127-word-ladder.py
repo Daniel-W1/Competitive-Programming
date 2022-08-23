7f12896109a89d9f1301b5c5ef20060c784b40e7
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-08-23 15:22:04 +0300

    Time: 134 ms (95.71%), Space: 17.7 MB (29.59%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -1,33 +1,48 @@
-class Solution:
-    def ladderLength(self, start: str, end: str, words: List[str]) -> int:
-        words = set(words)
-        letters = string.ascii_lowercase
-        
-        def findall(word):
-            ans = []
-            for i, letter in enumerate(word):
-                for char in letters:
-                    string = word[:i]+char+word[i+1:]
-                    if string in words:
-                        ans.append(string)
-                        words.remove(string)
-            return ans
-        
-        q = collections.deque([start])
-        moves = 1
-        
-        while q:
-            cur = len(q)
-            for _ in range(cur):
-                word = q.popleft()
-                if word == end:
-                    return moves
-                
-                res = findall(word)
-                q += res
-            moves += 1
+from collections import defaultdict
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        """
+
+        if endWord not in wordList or not endWord or not beginWord or not wordList:
+            return 0
+
+        # Since all words are of same length.
+        L = len(beginWord)
+
+        # Dictionary to hold combination of words that can be formed,
+        # from any given word. By changing one letter at a time.
+        all_combo_dict = defaultdict(list)
+        for word in wordList:
+            for i in range(L):
+                # Key is the generic word
+                # Value is a list of words which have the same intermediate generic word.
+                all_combo_dict[word[:i] + "*" + word[i+1:]].append(word)
+
+
+        # Queue for BFS
+        queue = collections.deque([(beginWord, 1)])
+        # Visited to make sure we don't repeat processing same word.
+        visited = {beginWord: True}
+        while queue:
+            current_word, level = queue.popleft()
+            for i in range(L):
+                # Intermediate words for current word
+                intermediate_word = current_word[:i] + "*" + current_word[i+1:]
+
+                # Next states are all the words which share the same intermediate state.
+                for word in all_combo_dict[intermediate_word]:
+                    # If at any point if we find what we are looking for
+                    # i.e. the end word - we can return with the answer.
+                    if word == endWord:
+                        return level + 1
+                    # Otherwise, add it to the BFS Queue. Also mark it visited
+                    if word not in visited:
+                        visited[word] = True
+                        queue.append((word, level + 1))
+                all_combo_dict[intermediate_word] = []
         return 0
-                    
-                
-                
-            
