e6f402bb5775e6917147dbde79c09f44b455cf35
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-09-08 14:49:09 +0300

    Time: 65 ms (13.55%), Space: 14 MB (19.72%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,19 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # ans = {}
-        # ans[len(nums)-1], ans[len(nums)-2] = nums[len(nums)-1], max(nums[len(nums)-1], nums[len(nums)-2])
-        # def smartrob(i):
-        #     if i not in ans:
-        #         ans[i] = max(smartrob(i+2) + nums[i], smartrob(i+1))
-        #     return ans[i]
-        # return smartrob(0)
-    # lol this looks like a bottom up with recursion but let's do a real bottom up huh?
-        ans = [0]*len(nums)
-        if len(ans)<2:
-            return nums[0]
-        ans[0], ans[1] = nums[0], max(nums[0], nums[1])
-        for i in range(2, len(nums)):
-            ans[i] = max(ans[i-2]+nums[i], ans[i-1])
-        return ans[-1]
-    
-    
+        
+        @cache
+        def dfs(idx):
+            if idx > len(nums)-1:
+                return 0
+            
+            choice1 = dfs(idx+2) + nums[idx]
+            choice2 = dfs(idx + 1)
+            
+            return max(choice1, choice2)
+        
+        return dfs(0)
