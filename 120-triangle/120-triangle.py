e0f01f82c00b62c8129edfe76f8746da8da5703c
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-09-08 16:00:12 +0300

    Time: 66 ms (97.16%), Space: 15.1 MB (36.17%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,6 +1,8 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        rowSize = len(triangle)
+        
+        '''
+         rowSize = len(triangle)
         
         @cache
         def dfs(row, idx):
@@ -15,3 +17,20 @@ class Solution:
             
             return min(choice1, choice2)
         return dfs(0,0)
+        
+        '''
+        dp = [[0 for _ in range(len(triangle[idx]))] for idx in range(len(triangle))]
+        dp[0][0] = triangle[0][0]
+        
+        for rowind in range(1, len(dp)):
+            row = dp[rowind]
+            for idx in range(len(row)):
+                if idx == 0: 
+                    dp[rowind][idx] = triangle[rowind][idx]+dp[rowind-1][idx]
+                elif idx == len(row)-1:
+                    dp[rowind][idx] = triangle[rowind][idx] + dp[rowind-1][idx-1]
+                else:
+                    dp[rowind][idx] = triangle[rowind][idx]+ min(dp[rowind-1][idx], dp[rowind-1][idx-1])
+        return min(dp[-1])
+    # time comp O(m*n)
+    # space comp O(m*n)
