9f06dca0f3ca39b1a51a2a5ed30c59907a8a7ac5
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-09-08 17:06:25 +0300

    Time: 59 ms (25.58%), Space: 13.8 MB (65.43%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,6 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
+        '''
         @cache
         def dfs(idx):
             if idx > len(nums)-1:
@@ -12,3 +13,16 @@ class Solution:
             return max(choice1, choice2)
         
         return dfs(0)
+        
+        '''
+        dp = [0]*len(nums)
+        dp[0] = nums[0]
+        
+        for idx in range(1, len(nums)):
+            if idx > 1:
+                dp[idx] = max(dp[idx-2]+nums[idx], dp[idx-1])
+            else:
+                dp[idx] = max(nums[idx], dp[idx-1])
+                
+        return dp[-1]
+        # time O(n), space O(n), space can be optimised to O(1)
