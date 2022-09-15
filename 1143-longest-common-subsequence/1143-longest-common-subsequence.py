b6ec51539faaa2accfecb8a00fc18a18b03d0d89
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-09-15 12:55:26 +0300

    Time: 1760 ms (11.21%), Space: 140.4 MB (11.85%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,14 +1,13 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        m,n = len(text1)-1,len(text2)-1
-        memo = {}
-        def findlongest(i,j):
-            if (i,j) in memo: return memo[i,j]
-            if i > m or j > n:
-                return 0
-            if text1[i] == text2[j]:
-                memo[i,j] = 1 + findlongest(i+1,j+1)
+        
+        @cache
+        def find(pointer1, pointer2):
+            if pointer1 == -1 or pointer2 == -1: return 0
+            
+            elif text1[pointer1] == text2[pointer2]:
+                return 1 + find(pointer1-1, pointer2-1)
             else:
-                memo[i,j] = max(findlongest(i+1,j),findlongest(i,j+1))
-            return memo[i,j]
-        return findlongest(0,0)
+                return max(find(pointer1-1, pointer2), find(pointer1, pointer2-1))
+            
+        return find(len(text1)-1, len(text2)-1)
