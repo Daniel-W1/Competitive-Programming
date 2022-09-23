d96adda8071bfbe5a10a7c3fa55f40f0a7b9c1b8
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-09-23 11:55:15 +0300

    Time: 506 ms (87.39%), Space: 18.7 MB (24.42%) - LeetHub

---
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,36 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        # it says longest increasing path
+        '''
+        so i think the right way to do this is just to write dfs
+        and memoise on the way
+        
+        and also memoise globally and use that memo for each row and col 
+        
+        '''
+        memo = {}
+        directions = [(1, 0), (-1, 0), (0, -1), (0,1)]
+        visited = [[False]*len(matrix[0]) for _ in range(len(matrix))]
+        
+        def dfs(row, col, visited):
+            if (row, col) in memo: return memo[row, col]
+            visited[row][col] = True
+            
+            res = 1
+            for dx, dy in directions:
+                newr, newc = row + dx, col + dy
+                if 0 <= newr < len(matrix) and 0 <= newc < len(matrix[0]):
+                    if not visited[newr][newc]:
+                        if matrix[newr][newc] > matrix[row][col]:
+                            res = max(res, dfs(newr, newc, visited)+1)
+            
+            visited[row][col] = False
+            memo[row, col] = res
+            return res
+        
+        ans = 0
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                ans = max(ans, dfs(row, col, visited))
+                
+        return ans
