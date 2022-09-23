4e254dc68c7548775a6b77b6ae2dd7467275e804
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-09-23 12:03:57 +0300

    Time: 492 ms (89.06%), Space: 18.5 MB (30.31%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -8,6 +8,7 @@ class Solution:
         and also memoise globally and use that memo for each row and col 
         
         '''
+        m, n = len(matrix), len(matrix[0])
         memo = {}
         directions = [(1, 0), (-1, 0), (0, -1), (0,1)]
         visited = [[False]*len(matrix[0]) for _ in range(len(matrix))]
@@ -19,7 +20,7 @@ class Solution:
             res = 1
             for dx, dy in directions:
                 newr, newc = row + dx, col + dy
-                if 0 <= newr < len(matrix) and 0 <= newc < len(matrix[0]):
+                if 0 <= newr < m and 0 <= newc < n:
                     if not visited[newr][newc]:
                         if matrix[newr][newc] > matrix[row][col]:
                             res = max(res, dfs(newr, newc, visited)+1)
@@ -28,9 +29,4 @@ class Solution:
             memo[row, col] = res
             return res
         
-        ans = 0
-        for row in range(len(matrix)):
-            for col in range(len(matrix[0])):
-                ans = max(ans, dfs(row, col, visited))
-                
-        return ans
+        return max(dfs(i, j, visited) for i, j in product(range(m), range(n)))
