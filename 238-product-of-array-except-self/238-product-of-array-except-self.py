db74b78a99f32b042d2e25d0e9c77112198d616c
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-09-23 09:34:15 +0300

    Time: 943 ms (5.01%), Space: 22.5 MB (18.18%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,25 +1,28 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix = [1]*len(nums)
-        postfix = [1]*len(nums)
-        ans = [1]*len(nums)
-        pre, post = 1,1
-        for i in range(len(nums)):
-            prefix[i] = pre*nums[i]
-            pre = prefix[i]
-       
+        '''
+        O(n) time without using any division operation right
         
-        for i in range(len(nums)-1, -1, -1):
-            postfix[i] = post*nums[i]
-            post = postfix[i]
+        prefix and suffix array
+        let's look at what the prefix array would look like
+        [1, 2, 6, 24]
+        [ 24, 24,12,4]
         
-        mul = 1
-        for i in range(len(nums)):
-            if i + 1 == len(nums):
-                ans[i] = mul*1
-            else:
-                ans[i] = mul*postfix[i+1]
-                mul = prefix[i]
+        so when i don't use a number the result is just the prefix before that
+        number time the suffix after that number because those totaly isolate the number
+        
+        '''
+        prefix = [nums[0]]*len(nums)
+        for idx in range(1, len(nums)):
+            prefix[idx] = nums[idx]*prefix[idx-1]
+        
+        suffix = [nums[-1]]*len(nums)
+        for idx in range(len(nums)-2, -1, -1):
+            suffix[idx] = nums[idx]*suffix[idx+1]
+        
+        ans = [0]*len(nums)
+        for idx in range(len(nums)):
+            left = prefix[idx-1] if idx -1 >=0 else 1
+            right = suffix[idx+1] if idx + 1 < len(nums) else 1
+            ans[idx] = left * right
         return ans
-        
-        
