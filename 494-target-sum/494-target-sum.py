51be1152e56485fa449eda296809415d99d0d34e
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-09-23 11:08:50 +0300

    Time: 857 ms (17.40%), Space: 41.7 MB (8.89%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -1,11 +1,28 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        memo = {}
-        def cntall(i,res):
-            if (i,res) not in memo:
-                if i > len(nums)-1:
-                    return 1 if res == target else 0
-                memo[i,res]= cntall(i+1,res+nums[i]) + cntall(i+1,res-nums[i])
-            return memo[i,res]
-        return cntall(0, 0)
-                
+        '''
+        so at each index i have a choice and i need to do one of the choice
+        
+        so i have 2 states idx, and cursum 
+        
+        at each index i make those choices and when the whole thing is over if
+        my sum is target i return 1 else 0
+        and just collect all those things to get the result
+        
+        
+                                    +(1) or -(-1)
+                                +(2)  (0) or   
+                                
+        '''
+        @cache
+        def check(idx, cursum):
+            if idx == len(nums):
+                return int(cursum == target)
+        
+            choice1 = check(idx+1, cursum + nums[idx])
+            choice2 = check(idx+1, cursum - nums[idx])
+            
+            return choice1 + choice2
+        
+        return check(0, 0)
+        
