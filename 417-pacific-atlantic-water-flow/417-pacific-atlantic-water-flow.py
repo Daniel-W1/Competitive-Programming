d0fab7aab4515bd0503179d036d8cfa3775e0d8f
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-09-25 07:42:26 +0300

    Time: 371 ms (76.90%), Space: 15.9 MB (22.73%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -5,39 +5,75 @@ class Solution:
         
         we need a visited set because we are also considering cells with the same heights
         '''
-        directions = [(1, 0), (0, 1), (-1, 0),(0, -1)]
-        m, n = len(heights), len(heights[0])
+#         directions = [(1, 0), (0, 1), (-1, 0),(0, -1)]
+#         m, n = len(heights), len(heights[0])
 
         
+
+#         def dfs(i, j, visited):
+#             nonlocal check1, check2
+            
+#             visited.add((i,j))
+            
+#             for dx, dy in directions:
+#                 newr, newc = i + dx, j + dy
+#                 if 0 <= newr < m and 0 <= newc < n:
+#                     if (newr, newc) not in visited:
+#                         if heights[newr][newc] <= heights[i][j]:
+#                             dfs(newr, newc, visited)
+#                 else:
+#                     if newr == m  or newc == n:
+#                         check1 = True
+#                     if newc == -1 or newr == -1:
+#                         check2 = True
+#         ans = []
         
-        def dfs(i, j, visited):
-            nonlocal check1, check2
-            
-            visited.add((i,j))
-            
+#         for row in range(m):
+#             for col in range(n):
+                
+#                 check1, check2 = False, False
+#                 dfs(row, col, set())
+                
+#                 if check1 and check2:
+#                     ans.append([row, col])
+#         return ans
+    
+#         # time comp is N^3 and space comp N^2 recursion stack
+        '''
+        this sol is very slow so how can we make it efficient
+        
+        I think we need to work on the efficiency of the sol
+        
+        we can dfs from the left and the right and record the pos that we see
+        '''
+        m, n = len(heights), len(heights[0])
+        directions = [(1, 0), (0, 1), (-1, 0),(0, -1)]
+
+        def dfs(i, j, seen):
+            seen.add((i,j))
             for dx, dy in directions:
                 newr, newc = i + dx, j + dy
                 if 0 <= newr < m and 0 <= newc < n:
-                    if (newr, newc) not in visited:
-                        if heights[newr][newc] <= heights[i][j]:
-                            dfs(newr, newc, visited)
-                else:
-                    if newr == m  or newc == n:
-                        check1 = True
-                    if newc == -1 or newr == -1:
-                        check2 = True
-        ans = []
+                    if (newr, newc) not in seen:
+                        if heights[newr][newc] >= heights[i][j]:
+                            dfs(newr, newc, seen)
         
-        for row in range(m):
-            for col in range(n):
-                
-                check1, check2 = False, False
-                dfs(row, col, set())
-                
-                if check1 and check2:
-                    ans.append([row, col])
-        return ans
-                    
+        # first let's go through the rows
+        pacific, atlantic = set(), set()
+        for idx in range(m):
+            dfs(idx, 0, pacific)
+            dfs(idx, n-1, atlantic)
+        
+        # first let's go through the cols
+        for idx in range(n):
+            dfs(0, idx, pacific)
+            dfs(m-1, idx, atlantic)
+        
+        both = pacific & atlantic
+        return [[row, col] for row, col in both]
+            
+        
+        
                         
                             
                 
