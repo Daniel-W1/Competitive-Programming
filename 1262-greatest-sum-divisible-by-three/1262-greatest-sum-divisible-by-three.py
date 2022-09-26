fe698745cbb42a4bfe1a506b238a4aa242f5956b
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-09-26 17:03:42 +0300

    Time: 530 ms (67.10%), Space: 20.5 MB (36.77%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
@@ -16,44 +16,35 @@ class Solution:
         ans = 0
         for num in nums:
             if num%3 == 1:
-                ones.append(num)
+                if len(ones) < 2:
+                    heappush(ones, -num)
+                else:
+                    heappushpop(ones, -num)
+                    
             elif num%3 == 2:
-                twos.append(num)
-            else:
-                ans += num
-        
-        
-            
-#         while len(ones) >= 3:
-#             for _ in range(3):
-#                 ans += -heappop(ones)
-#         while len(twos) >= 3:
-#             for _ in range(3):
-#                 ans += -heappop(twos)
-                
-#         while ones and twos:
-#             one, two = -heappop(ones), -heappop(twos)
-#             ans += one + two
-        res = sum(ones) + sum(twos)
-        ans += res
-        
-        # print(ans, ans%3)
-        # print(ones, twos)
+                if len(twos) < 2:
+                    heappush(twos, -num)
+                else:
+                    heappushpop(twos, -num)
+            ans += num
         
         choice1, choice2 = float('inf'), float('inf')
+        # print(ones, twos, ans%3)
         if ans%3 == 1:
             if ones:
-                choice1 = min(ones)
+                choice1 = -max(ones)
             if len(twos) > 1:
-                choice2 = sum(sorted(twos)[:2])
+                choice2 = -sum(twos)
+                
             ans -= min(choice1, choice2)
         elif ans%3 == 2:
             if len(ones) > 1:
-                choice1 = sum(sorted(ones)[:2])
+                choice1 = -sum(ones)
             if twos:
-                choice2 = min(twos)
+                choice2 = -max(twos)
             
             ans -= min(choice1, choice2)
-        return ans if not (ans)%3 else 0
+        
+        return ans if not ans%3 else 0
         
         
