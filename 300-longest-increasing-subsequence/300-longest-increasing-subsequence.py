2f9c2b308f32107ad0596fcbe0335b0eba074209
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-09-26 12:17:44 +0300

    Time: 166 ms (69.34%), Space: 14.3 MB (46.34%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,12 +1,27 @@
-class Solution { // 256 ms, faster than 42.84%
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, 1);
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < i; ++j)
-                if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
-                    dp[i] = dp[j] + 1;
-        return *max_element(dp.begin(), dp.end());
-    }
-};
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        temp = [nums[0]]
+        
+        '''
+        [0,1,0,3,2,3]
+        
+        0, 1, 2, 3 
+        
+        [7,7,7,7,7,7]
+        
+        
+        [10,9,2,5,3,7,101,18]
+        
+        2 3  
+        '''
+        for idx in range(1, len(nums)):
+            num = nums[idx]
+            insert = bisect_left(temp, num)
+            
+            if insert == len(temp):
+                temp.append(num)
+            
+            elif temp[insert] > num:
+                temp[insert] = num
+        
+        return len(temp)
