d5ad48f3347cf9a7789b95e296e3a1ef53a6a71d
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-09-28 14:29:49 +0300

    Time: 66 ms (26.16%), Space: 14 MB (20.71%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -5,6 +5,27 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        prev_to_first = None
+        first = head
+        second = head
+        n -= 1
+        while n > 0:
+            second = second.next
+            n -= 1
+
+        while second.next:
+            prev_to_first = first
+            first = first.next
+            second = second.next
+        
+        # print(first.val)
+        if not prev_to_first:
+            prev_to_first = first.next
+            return prev_to_first
+        else:
+            prev_to_first.next = prev_to_first.next.next
+
+        return head
         '''
         the most obvious way is to reverse it and then remove the nth node 
         then reverse it back again
@@ -17,30 +38,72 @@ class Solution:
         
         '''
         
-        def reverse(node):
-            prev = None
-            cur = node
-            while cur:
-                the_next = cur.next
-                cur.next = prev
-                cur, prev = the_next, cur
+#         def reverse(node):
+#             prev = None
+#             cur = node
+#             while cur:
+#                 the_next = cur.next
+#                 cur.next = prev
+#                 cur, prev = the_next, cur
             
-            return prev
+#             return prev
         
-        n -= 2
-        new_head = cur = reverse(head)
+#         n -= 2
+#         new_head = cur = reverse(head)
         
-        if n < 0: 
-            first_node = ListNode()
-            first_node.next = cur.next
-            return reverse(first_node.next)
+#         if n < 0: 
+#             first_node = ListNode()
+#             first_node.next = cur.next
+#             return reverse(first_node.next)
         
-        while n > 0:
-            cur = cur.next
-            n -= 1
+#         while n > 0:
+#             cur = cur.next
+#             n -= 1
             
-        cur.next = cur.next.next
-        return reverse(new_head)
+#         cur.next = cur.next.next
+#         return reverse(new_head)
+    
+    # but the time and space comp is O(n) and space is O(1)
+    
+    # now let's try to do it in one pass
+    # when ever u want the smart sol using linked lists we need to think of two pointers 
+    
+    '''
+    first = head
+    second = head + (k-1) nodes
+    keep track of prev to the first node and when u delete delete the first node
+    
+    '''
+    
+        
+        
+        
             
-            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
