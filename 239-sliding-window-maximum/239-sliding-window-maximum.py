b73f83246ab918667c7a689c82d33f3d6d4899eb
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-09-29 12:08:09 +0300

    Time: 3836 ms (32.91%), Space: 30.1 MB (45.53%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,23 +1,28 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        left, right = 0, 0
-        heap = []
-        removed = set()
-        while k > 1:
-            heapq.heappush(heap, (-nums[right], right))
-            right += 1
-            k -= 1
+        left = 0
+        q = deque([])
         
+        for right in range(k):
+            while q and q[-1][0] <= nums[right]:
+                q.pop()
+            q.append((nums[right], right))
+    
         ans = []
-        for idx in range(right, len(nums)):
-            heapq.heappush(heap, (-nums[idx], idx))
-            while heap[0][1] in removed:
-                heapq.heappop(heap)
-            ans.append(-heap[0][0])
-            removed.add(left)
+        for idx in range(right+1, len(nums)):
+            # print(q)
+            ans.append(q[0][0])
             left += 1
-        return ans
-    # time comp O(nlogn)
-    # space O(n)
+            while q and q[-1][0] <= nums[idx]:
+                q.pop()
+            q.append((nums[idx], idx))
             
+            if q[0][1] < left:
+                q.popleft()
+                
+        ans.append(q[0][0])
+        return ans
+    # time comp O(nlogk)
+    # space O(n)
+    # try the O(n) time sol tomorrow
             
