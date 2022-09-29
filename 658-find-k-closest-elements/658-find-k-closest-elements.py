084cf0aa6afa65a0cb7157dcc0e27a0ca2599835
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-09-29 06:29:50 +0300

    Time: 666 ms (30.85%), Space: 15.4 MB (98.39%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.py
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+    def findClosestElements(self, A: List[int], k: int, x: int) -> List[int]:
         '''
         
         [ -1, 1, 2, 4,5, 7, 12, 14], k = 5, x = 15
@@ -10,40 +10,12 @@ class Solution:
         
         1, 2, 3, 12, 15, 15, k = 4, x = 3
         '''
-        insert_pos = bisect_left(arr, x)
-        
-        # edge cases 
-        if insert_pos == 0: return arr[:k]
-        if insert_pos == len(arr): return arr[-k:]
-        
-        left, right = insert_pos - 1, insert_pos 
-        ans = deque([])
-        
-        while left >= 0 and right < len(arr):
-            num1 = arr[left]
-            num2 = arr[right]
-            
-            dist1 = abs(x - num1)
-            dist2 = abs(x - num2)
-            
-            if len(ans) == k: break
-            
-            if dist1 <= dist2:
-                ans.appendleft(num1)
-                left -= 1
+        left, right = 0, len(A) - k
+        while left < right:
+            mid = (left + right) // 2
+            if x - A[mid] > A[mid + k] - x:
+                left = mid + 1
             else:
-                ans.append(num2)
-                right += 1
-        
-        while len(ans) < k:
-            if left >= 0:
-                ans.appendleft(arr[left])
-                left -= 1
-            elif right < len(arr):
-                ans.append(arr[right])
-                right += 1
-        
-        # print(len(ans))
-        return ans
-        
+                right = mid
+        return A[left:left + k]
         
