303eec8412624cf313539c62e095175cabccc50e
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-01 10:56:02 +0300

    Time: 30 ms (97.47%), Space: 14.2 MB (10.63%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,16 +1,45 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        possible_range = range(1, 27)
-        memo = {}
-        def decode(i,cur):
-            if (cur,len(s)-i - 1) not in memo:
-                if i == len(s)-1 and int(cur) in possible_range:
-                    return 1
-                if int(cur) not in possible_range:
-                    return 0
-                if int(cur) in possible_range:
-                    memo[cur, len(s)-i-1] = decode(i+1, s[i+1]) + decode(i+1,cur+s[i+1])
-            return memo[cur,len(s)-i-1]
+        
+        '''
+        226
+        [1, 2, 3]
+        
+        1111
+        [1, 2, 3,]
+        
+        at each index their are choices i need to make for example 
+        if the cur num is 1 we definetly have two choices 
+        
+        if the cur is 2 and the next is <= 6 we will have two choices
+        
+        else we only have on choices
+        
+        
+        that makes sense but how are u going to handle the 0s
+        
+        if we find a zero we will look at the value we get before 0 and 
+        if that value with 0 gives us a valid thing then the whole thing 
+        must be valid
+        
+        my state is one and time comp 2^n am making 2 decision at each index
+        O(N)
+        
+        '''
+        @cache
+        def dfs(idx):
+            if idx >= len(s): return 1
             
-        return decode(0,s[0])
+            if s[idx] == "1" and idx + 1 < len(s):
+                return dfs(idx + 1) + dfs(idx + 2)
             
+            elif s[idx] == "2" and idx + 1 < len(s) and int(s[idx+1]) <= 6:
+                return dfs(idx + 1) + dfs(idx + 2)
+        
+            elif int(s[idx]) >= 1:
+                return dfs(idx+1)
+            else:
+                return 0
+        return dfs(0)
+            
+                
