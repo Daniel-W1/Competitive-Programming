28f095975a46e6b921a000e92a566807da3feea5
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-03 12:21:05 +0300

    Time: 435 ms (65.36%), Space: 14.1 MB (87.14%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -1,15 +1,40 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        real_target = (sum(nums) - target)/2
+        '''
+        this is counting dp and here is how we are going to do it
         
-        @cache
-        def dfs(idx, cursum):
-            if idx >= len(nums):
-                return int(cursum == real_target)
-            
-            choice1 = dfs(idx + 1, cursum)
-            choice2 = dfs(idx + 1, cursum  + nums[idx])
-            
-            return choice1 + choice2
+        s1 - s2 = target 
+        s1 = total - s2
+        total - s2 - s2 = target
+        total - target = 2*s2
+        s2 = (total - target)/2
         
-        return dfs(0, 0)
+        
+        '''
+        total = sum(nums)
+        s2 = (total - target)/2
+    
+        
+        if int(s2) != s2 or s2 < 0: return 0
+        
+        dp = [[0 for _ in range(int(s2+1))] for _ in range(len(nums))]
+        
+        # print(s2)
+        # base cases
+        for row in range(len(nums)):
+            dp[row][0] = 1
+        
+        if nums[0] <= s2:
+            dp[0][nums[0]] += 1
+        
+        for row in range(1,len(nums)):
+            for col in range(int(s2 + 1)):
+                if nums[row] > col:
+                    dp[row][col] = dp[row-1][col]
+                else:
+                    # print(row, col)
+                    dp[row][col] = dp[row-1][col] + dp[row-1][col - nums[row]]
+        
+        # print(dp)
+
+        return dp[-1][-1]
