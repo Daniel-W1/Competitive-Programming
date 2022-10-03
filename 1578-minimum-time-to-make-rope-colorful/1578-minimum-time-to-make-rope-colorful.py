a7f91d7d168552e9d1b4cc07d63a6b8a9aaea83c
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-03 08:10:27 +0300

    Time: 2370 ms (38.51%), Space: 25 MB (52.92%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -1,21 +1,25 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        # totalTime: total time needed to make rope colorful;
-        # currMaxTime: maximum time of a balloon needed in this group.
-        total_time = 0
-        curr_max_time = 0
+        '''
+        aaa
+        | |
         
-        # For each balloon in the array:
-        for i in range(len(colors)):
-            # If this balloon is the first balloon of a new group
-            # set the curr_max_time as 0.
-            if i > 0 and colors[i] != colors[i - 1]:
-                curr_max_time = 0
-            
-            # Increment total_time by the smaller one.
-            # Update curr_max_time as the larger one.
-            total_time += min(curr_max_time, neededTime[i])
-            curr_max_time = max(curr_max_time, neededTime[i])
+        
+        Input: colors = "aabaa", neededTime = [1,2,3,4,1]
+                            ||
+        '''
+        left = 0
+        cost = 0
+        
+        for right in range(1, len(colors)):
+            if colors[left] == colors[right]:
+                cost += min(neededTime[left], neededTime[right])
+    
+                if neededTime[left] < neededTime[right]:
+                    left = right
                 
-        # Return total_time as the minimum removal time.
-        return total_time
+            else:
+                left = right
+    
+        return cost
+            
