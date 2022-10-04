bc8ea6c552a383a791fee366b62426a62a65021d
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-04 10:14:22 +0300

    Time: 1652 ms (16.60%), Space: 42.7 MB (20.50%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.py
+++ 518-coin-change-ii/518-coin-change-ii.py
@@ -2,16 +2,16 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         
         coins.sort(reverse = True)
+        
         @cache
-        def dfs(left, target):
+        def dfs(start, target):
             if target <= 0:
                 return int(target == 0)
             
             res = 0
-            for idx in range(left, len(coins)):
+            for idx in range(start, len(coins)):
                 coin = coins[idx]
-                res += dfs(left, target - coin)
-                left += 1
+                res += dfs(idx, target - coin)
                 
             return res
         
