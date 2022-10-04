114bbafd212ba6c2b0a6dce5e2ef5056bb3e6126
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-04 07:49:50 +0300

    Time: 66 ms (37.66%), Space: 14.1 MB (27.97%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        def backtrack(nums):
+            nums = set(nums)
+            check = False
+            unique = ""
+
+            def dfs(n, curstring): # n = 3
+                nonlocal check, unique
+                if n == 0:
+                    if curstring not in nums: 
+                        check = True
+                        unique = curstring
+                    return 
+
+                if not check:
+                  dfs(n - 1, curstring + "1")
+                if not check:
+                  dfs(n - 1, curstring + "0")
+            dfs(len(nums), "")
+            return unique
+        return backtrack(nums)
