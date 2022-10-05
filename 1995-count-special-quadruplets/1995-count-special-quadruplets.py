1e232832133bb5f3e15befcf66bce03cbd4b93b7
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-05 16:44:10 +0300

    Time: 169 ms (81.21%), Space: 14 MB (21.41%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.py
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.py
@@ -1,8 +1,15 @@
 class Solution:
     def countQuadruplets(self, nums: List[int]) -> int:
-        ans = 0 
-        freq = Counter()
-        for i in range(len(nums)): 
-            for j in range(i+1, len(nums)): ans += freq[nums[j] - nums[i]]
-            for ii in range(i): freq[nums[ii] + nums[i]] += 1
+        
+        cnt = {}
+        ans = 0
+        
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                ans += cnt.get(nums[j] - nums[i], 0)
+                
+            for k in range(i):
+                cnt[nums[k] + nums[i]] = cnt.get(nums[k] + nums[i], 0) + 1
+        
         return ans
+                
