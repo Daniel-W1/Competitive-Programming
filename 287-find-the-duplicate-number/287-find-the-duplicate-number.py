68d7ea580e184fe578e03ca272deaedea4db6913
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-06 11:15:33 +0300

    Time: 5123 ms (5.00%), Space: 28 MB (57.78%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,17 +1,24 @@
 class Solution:
-    def findDuplicate(self, nums):
-        # Find the intersection point of the two runners.
-        tortoise = hare = nums[0]
-        while True:
-            tortoise = nums[tortoise]
-            hare = nums[nums[hare]]
-            if tortoise == hare:
-                break
-        
-        # Find the "entrance" to the cycle.
-        tortoise = nums[0]
-        while tortoise != hare:
-            tortoise = nums[tortoise]
-            hare = nums[hare]
-        
-        return hare
+    def findDuplicate(self, nums: List[int]) -> int:
+        duplicate = 0
+        n = len(nums) - 1
+        bits = n.bit_length()
+        for bit in range(bits):
+            mask = 1 << bit
+            base_count = 0
+            nums_count = 0
+            for i in range(n + 1):
+                # If bit is set in number (i) then add 1 to base_count
+                if i & mask:
+                    base_count += 1
+                    
+                # If bit is set in nums[i] then add 1 to nums_count
+                if nums[i] & mask:
+                    nums_count += 1
+                    
+            # If the current bit is more frequently set in nums than it is in 
+            # the range [1, 2, ..., n] then it must also be set in the duplicate number.
+            if nums_count - base_count > 0:
+                duplicate |= mask
+                
+        return duplicate
