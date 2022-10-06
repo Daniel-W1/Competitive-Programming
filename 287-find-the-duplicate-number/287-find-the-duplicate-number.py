d16a2a8258dd3e74b0b28322b1415c92e9e273cf
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-06 11:12:04 +0300

    Time: 1754 ms (11.03%), Space: 27.8 MB (91.09%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,20 +1,17 @@
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        left, right = 1, len(nums)
+    def findDuplicate(self, nums):
+        # Find the intersection point of the two runners.
+        tortoise = hare = nums[0]
+        while True:
+            tortoise = nums[tortoise]
+            hare = nums[nums[hare]]
+            if tortoise == hare:
+                break
         
+        # Find the "entrance" to the cycle.
+        tortoise = nums[0]
+        while tortoise != hare:
+            tortoise = nums[tortoise]
+            hare = nums[hare]
         
-        while left <= right:
-            mid = (left + right)//2
-            cnt = 0
-            for num in nums:
-                if num <= mid:
-                    cnt += 1
-            
-            if cnt > mid:
-                right = mid - 1
-            else:
-                left = mid + 1
-        
-        return left
-                
-        
+        return hare
