955171eeabe8733ddb3b6b739c45b9bfe2d6accc
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-07 07:43:33 +0300

    Time: 31 ms (93.02%), Space: 13.8 MB (78.86%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.py
+++ 556-next-greater-element-iii/556-next-greater-element-iii.py
@@ -16,7 +16,7 @@ class Solution:
         if not last:
             return -1
         
-        nums = nums[:idx+1] + sorted(nums[idx+1:])
+        nums = nums[:idx+1] + nums[idx+1:][::-1]
         res = int("".join(nums)) 
         
         return res if res <= 2**31-1 else -1
