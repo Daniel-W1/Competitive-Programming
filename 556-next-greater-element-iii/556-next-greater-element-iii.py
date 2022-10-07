eba6cdc7091c91ddaf3a1dff886dc857dd015b48
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-07 07:39:50 +0300

    Time: 40 ms (76.21%), Space: 13.8 MB (78.86%) - LeetHub

---
+++ 556-next-greater-element-iii/556-next-greater-element-iii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def nextGreaterElement(self, n: int) -> int:
+        nums = list(str(n))
+        stack = []
+        last = None
+        
+        for idx in range(len(nums)-1, -1, -1):
+            while stack and nums[stack[-1]] > nums[idx]:
+                last = stack.pop()
+            
+            if last:
+                nums[idx], nums[last] = nums[last], nums[idx]
+                break
+            stack.append(idx)
+        
+        if not last:
+            return -1
+        
+        nums = nums[:idx+1] + sorted(nums[idx+1:])
+        res = int("".join(nums)) 
+        
+        return res if res <= 2**31-1 else -1
+            
+        
+        
