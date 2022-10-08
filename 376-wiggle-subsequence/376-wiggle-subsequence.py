93e62b7c27a506f5783e56c64581840831a76c04
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-08 16:32:51 +0300

    Time: 32 ms (98.24%), Space: 14 MB (9.56%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.py
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -1,33 +1,16 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
-        max_len = 0
-        ans = []
-        
-        if len(nums) == 1:
-            return len(nums)
-        
-        for i in range(len(nums)):
-            if i + 1 == len(nums): break
-            in_dif = (nums[i+1] - nums[i])
-            
-            if in_dif: in_dif /= abs(in_dif)
-            else: continue
-                
-            ans = nums[i:i+2]
-            if max_len < len(nums) - i:
-                index = i+2
-                while index < len(nums):
-                    check_dif = (nums[index] - nums[index-1])
-                    if check_dif: check_dif /= abs(check_dif)
-                    if check_dif == -1*in_dif:
-                        ans.append(nums[index])
-                    else:
-                        index += 1
-                        continue
-                    index += 1
-                    in_dif = check_dif
-                max_len = max(max_len, len(ans))
-                        
-        return max_len if ans else 1
-            
-        
+        if not nums:
+            return 0
+        inc, dec = [1] * len(nums), [1] * len(nums)
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                inc[i] = dec[i - 1] + 1
+                dec[i] = dec[i - 1]
+            elif nums[i] < nums[i - 1]:
+                dec[i] = inc[i - 1] + 1
+                inc[i] = inc[i - 1]
+            else:
+                inc[i] = inc[i - 1]
+                dec[i] = dec[i - 1]
+        return max(max(inc), max(dec))
