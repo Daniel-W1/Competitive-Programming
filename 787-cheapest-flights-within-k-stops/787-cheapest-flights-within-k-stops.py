64f81066870dc7cef1f2d62979d1476a846a1c89
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-09 16:26:02 +0300

    Time: 117 ms (93.54%), Space: 15.4 MB (36.67%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -1,16 +1,30 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
 
-        adjacentMatrix, visited = [[0] * n for _ in range(n)], [-1] * n
-        for from_, to, price in flights:
-            adjacentMatrix[from_][to] = price
-        pq = [[0, src, k+1]]
-        while pq:
-            currentPrice , currentNode, currentStep = heappop(pq)
-            if currentNode == dst: return currentPrice
-            if currentStep == 0: continue
-            visited[currentNode] = currentStep
-            for idx, neighborPrice in enumerate(adjacentMatrix[currentNode]):
-                if neighborPrice and currentStep > visited[idx]:
-                    heappush(pq, [neighborPrice + currentPrice, idx, currentStep - 1])
-        return -1
+        graph = defaultdict(list)
+        for n1, n2, p in flights:
+            graph[n1].append((n2, p))
+        
+        # print(graph)
+        visited  = [-1 for _ in range(n)]
+        def shortest(src, dst, k):
+            heap = [(0, src, k+1)]
+            while heap:
+                price, node, k = heappop(heap)
+                    
+                if k < 0:
+                    continue
+                
+                if node == dst:
+                    return price
+                
+                visited[node] = k
+                
+                for other, p in graph[node]:
+                    if k > visited[other]:
+                        heappush(heap, (p+price, other, k-1))
+                # print(heap)
+            
+            return -1
+        
+        return shortest(src, dst, k)
