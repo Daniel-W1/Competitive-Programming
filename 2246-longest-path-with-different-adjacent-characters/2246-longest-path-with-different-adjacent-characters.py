a4101f99e671da0ca243deb7bca62439e19a5e54
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-11 00:54:14 +0300

    Time: 1660 ms (97.88%), Space: 151.9 MB (55.97%) - LeetHub

---
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestPath(self, parent: List[int], s: str) -> int:
+        tree = defaultdict(list)
+        for idx in range(1, len(parent)):
+            p = parent[idx]
+            tree[p].append(idx)
+        
+        # print(tree)
+        self.ans = 0
+        def dfs(root, parent):
+            if root not in tree:
+                return 1 if s[root] != s[parent] else 0
+            
+            res1, res2 = 0, 0
+            for child in tree[root]:
+                res = dfs(child, root) 
+                if res > res1: 
+                    res2, res1 = res1, res
+                elif res > res2: res2 = res
+            
+            # print(res1, res2, root, parent)
+            self.ans = max(self.ans, res1 + res2 + 1)
+            return max(res1, res2)+1 if s[root] != s[parent] else 0
+        
+        dfs(0, -1)
+        return max(self.ans, 1)
