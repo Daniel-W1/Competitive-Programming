9cc2534de3680d80635616647b9dd2a59dbc1bd0
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-10 16:21:39 +0300

    Time: 508 ms (87.24%), Space: 16.1 MB (55.02%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -1,12 +1,27 @@
 class Solution:
-    def networkDelayTime(self, times, N, K):
-        q, t, adj = [(0, K)], {}, collections.defaultdict(list)
-        for u, v, w in times:
-            adj[u].append((v, w))
-        while q:
-            time, node = heapq.heappop(q)
-            if node not in t:
-                t[node] = time
-                for v, w in adj[node]:
-                    heapq.heappush(q, (time + w, v))
-        return max(t.values()) if len(t) == N else -1
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)
+
+        for n1, n2, w in times:
+            graph[n1].append((n2, w))
+        
+        heap = [(0, k)]
+        ans = -float('inf')
+        visited = set()
+        
+        while heap:
+            time, cur = heappop(heap)
+            ans = max(ans, time)
+    
+            visited.add(cur)
+            
+            if len(visited) == n:
+                return ans
+            
+            for neigh, w in graph[cur]:
+                if neigh not in visited:
+                    heappush(heap, (time + w, neigh))
+        
+        return -1
+
+        
