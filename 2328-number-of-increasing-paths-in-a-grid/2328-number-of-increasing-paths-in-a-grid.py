0dbf83e23af10c1ea520fccd8ab4f4d89cf20528
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-13 08:48:22 +0300

    Time: 6043 ms (29.12%), Space: 117.7 MB (20.86%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
@@ -4,19 +4,17 @@ class Solution:
         directions = [(1, 0), (-1, 0), (0, -1), (0,1)]
         mod = 10**9 + 7
         
+        @cache
         def dfs(row, col):
-            if (row, col) in memo: return memo[row, col]
             res = 1
             for dx, dy in directions:
                 newr, newc = row + dx, col + dy
                 if 0 <= newr < m and 0 <= newc < n:
                     if matrix[newr][newc] > matrix[row][col]:
                         res += dfs(newr, newc)
-            memo[row, col] = res
             return res
         
         ans = 0
-        memo = {}
         
         for r in range(m):
             for c in range(n):
