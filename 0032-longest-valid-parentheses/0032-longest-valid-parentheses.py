0f5436baf74313e77a59340bcdfc903519e75264
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-14 08:03:17 +0300

    Time: 104 ms (24.12%), Space: 14.1 MB (78.53%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -1,23 +1,16 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
         dp = [0]*len(s)
-        for idx, char in enumerate(s):
-            if char == ")":
-                if idx >= 1 and s[idx-1] == "(":
+        
+        for idx in range(len(s)):
+            if s[idx] == ")":
+                if idx - 1 >= 0 and s[idx-1] =="(":
                     left = dp[idx - 2] if idx - 2 >= 0 else 0
                     dp[idx] = left + 2
-                elif idx >= 1 and s[idx-1] == ")":
-                    if dp[idx-1] == 0:
-                        dp[idx-1] = 0
-                    else:
-                        last = idx - dp[idx-1] - 1
-                        if last >= 0 and s[last] == "(":
-                            left = dp[last-1] if last - 1 >= 0 else 0
-                            dp[idx] = dp[idx-1] + left + 2
-
-        # print(dp)
+                
+                elif dp[idx-1] != 0 and idx - dp[idx-1] - 1 >= 0:
+                    if s[idx - dp[idx-1]-1] == "(":
+                        left = dp[idx - dp[idx-1]-2] if idx - dp[idx-1] - 2>=0 else 0                          
+                        dp[idx] = left + dp[idx-1] + 2
         return max(dp) if dp else 0
                     
-                    
-            
-            
