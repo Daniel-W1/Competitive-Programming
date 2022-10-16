3d6fcf76b94bee96b3ff49aa3f1fdc5fce36494c
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-16 16:38:57 +0300

    Time: 5760 ms (28.57%), Space: 16.5 MB (47.62%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.py
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -1,36 +1,10 @@
-class Solution:
-    def stoneGameIII(self, stoneValue: List[int]) -> str:
-        @cache
-        def dfs(idx, turn):
-            if idx >= len(stoneValue):
-                return 0
-            
-            if turn:
-                choice1 = dfs(idx+1, 1 - turn) + stoneValue[idx]
-                choice2, choice3 = -float('inf') ,-float('inf')
-                if idx + 1 < len(stoneValue):
-                    choice2 = dfs(idx + 2, 1 - turn) + stoneValue[idx] + stoneValue[idx+1]
-                if idx + 2 < len(stoneValue):
-                    choice3 = dfs(idx + 3, 1 - turn) + stoneValue[idx] + stoneValue[idx+1] + stoneValue[idx+2]
-                
-                return max(choice1, choice2, choice3)
-            
-            else:
-                choice1 = dfs(idx+1, 1 - turn)
-                choice2, choice3 = float('inf'), float('inf')
-                if idx + 1 < len(stoneValue):
-                    choice2 = dfs(idx + 2, 1 - turn)
-                if idx + 2 < len(stoneValue):
-                    choice3 = dfs(idx + 3, 1 - turn)
-                return min(choice1, choice2, choice3)
-        
-        res = dfs(0, 1)
-        # print(res)
-        if res == sum(stoneValue)-res:
-            return "Tie"
-        elif res > sum(stoneValue)-res:
-            return "Alice"
-        return "Bob"
-                
-                
-                
+class Solution(object):
+    def stoneGameIII(self, A):
+        """
+        :type stoneValue: List[int]
+        :rtype: str
+        """
+        dp = [0] * 3
+        for i in xrange(len(A) - 1, -1, -1):
+            dp[i % 3] = max(sum(A[i:i + k]) - dp[(i + k) % 3] for k in (1, 2, 3))
+        return ["Tie", "Alice", "Bob"][cmp(dp[0], 0)]
