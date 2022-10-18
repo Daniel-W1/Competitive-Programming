b6fe463e527dbacb2a6a112aeea0f8bda0d4705f
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-18 15:45:09 +0300

    Time: 33 ms (94.14%), Space: 14 MB (9.41%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -5,46 +5,38 @@ class Solution:
         
         return 0 <= row < m and 0 <= col < n
     
-    def fill(self,direction):
-        row, col = self.row, self.col 
-        
-        while self.inbound(row, col, self.grid) and self.grid[row][col] == 0:
-            self.grid[row][col] = self.curnum
+    def fill(self,direction, row, col, grid, curnum):
+        while self.inbound(row, col, grid) and grid[row][col] == 0:
+            grid[row][col] = curnum
             row += direction[0]
             col += direction[1]
-            self.curnum += 1
+            curnum += 1
             self.filledcnt += 1
-            # print(self.grid)
         
-        self.row = row - direction[0]
-        self.col = col - direction[1]
+        row -= direction[0]
+        col -= direction[1]
+        
+        return row, col, curnum
         
     def generateMatrix(self, n: int) -> List[List[int]]:
-        '''
-        1, 2, 3, 4, 5
-                    6
-                    7
-                    8
-        13   12   11  10
-        '''
         self.filledcnt = 0
-        self.grid = [[0 for col in range(n)] for row in range(n)]
+        grid = [[0 for col in range(n)] for row in range(n)]
         directions = [(0, 1),(1, 0), (0, -1), (-1, 0)]
-        current_direction = 0
-        self.row = 0
-        self.col = 0
-        self.curnum = 1
+        current_idx = 0
+        row = 0
+        col = 0
+        current_num = 1
         
         while self.filledcnt < n**2:
-            direction = directions[current_direction]
-            self.fill(direction)
-            current_direction += 1
-            current_direction %= len(directions)
-            direction = directions[current_direction]
-            self.row += direction[0]
-            self.col += direction[1]
+            direction = directions[current_idx]
+            row, col, current_num = self.fill(direction, row, col, grid, current_num)
+            current_idx += 1
+            current_idx %= len(directions)
+            direction = directions[current_idx]
+            row += direction[0]
+            col += direction[1]
         
-        return self.grid
+        return grid
             
             
             
