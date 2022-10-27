09aeeb0fe75171ce4e69b779f3470483c8627bbc
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-10-27 15:30:06 +0300

    Time: 2289 ms (84.08%), Space: 92.1 MB (58.89%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -2,29 +2,59 @@ class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         # so let's try to do this problem using MST
         # first we need to build the graph
-        graph = defaultdict(list)
-        n = len(points)
+        
         manhattan = lambda p1, p2 : abs(p1[0]-p2[0]) + abs(p1[1] - p2[1])
+        edges = []
         
-        for idx in range(n):
-            for other in range(idx + 1, n):
-                graph[idx].append((manhattan(points[idx], points[other]), other))
-                graph[other].append((manhattan(points[idx], points[other]), idx))
+        for idx in range(len(points)):
+            for other in range(idx + 1, len(points)):
+                p1, p2 = points[idx], points[other]
+                edges.append((idx, other, manhattan(p1, p2)))
         
-        visited = set()
-        minheap = [(0, 0)]
-        ans = 0
-        
-        while len(visited) < n:
-            cost, idx = heappop(minheap)
+        parents = list(range(len(points)))
+        def find(node):
+            if parents[node] != node:
+                parents[node] = find(parents[node])
             
-            if idx in visited:
+            return parents[node]
+        
+        cost = 0
+        for idx1, idx2, weight in sorted(edges, key = lambda edge : edge[2]):
+            p1, p2 = find(idx1), find(idx2)
+            
+            if p1 == p2:
                 continue
-            visited.add(idx)
-            ans += cost
             
-            for ncost, neighbor in graph[idx]:
-                if neighbor not in visited:
-                    heappush(minheap, (ncost, neighbor))
+            parents[p1] = p2
+            cost += weight
         
-        return ans
+        return cost
+            
+        
+
+#         graph = defaultdict(list)
+#         n = len(points)
+#         manhattan = lambda p1, p2 : abs(p1[0]-p2[0]) + abs(p1[1] - p2[1])
+        
+#         for idx in range(n):
+#             for other in range(idx + 1, n):
+#                 graph[idx].append((manhattan(points[idx], points[other]), other))
+#                 graph[other].append((manhattan(points[idx], points[other]), idx))
+        
+#         visited = set()
+#         minheap = [(0, 0)]
+#         ans = 0
+        
+#         while len(visited) < n:
+#             cost, idx = heappop(minheap)
+            
+#             if idx in visited:
+#                 continue
+#             visited.add(idx)
+#             ans += cost
+            
+#             for ncost, neighbor in graph[idx]:
+#                 if neighbor not in visited:
+#                     heappush(minheap, (ncost, neighbor))
+        
+#         return ans
