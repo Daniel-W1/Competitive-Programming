27540b60cba74b8bc596dce6286df96fb295f840
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-11-01 22:35:54 +0300

    Time: 911 ms (70.92%), Space: 71.3 MB (42.19%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,8 +1,7 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        
+        '''
         n1, n2 = len(s), len(t)
-        
         @cache
         def dfs(idx1, idx2):
             if idx2 == n2:
@@ -17,7 +16,29 @@ class Solution:
             return dfs(idx1 + 1, idx2)
         
         return dfs(0, 0)
+        '''
+        n1, n2 = len(s), len(t)
+        dp = [[0 for _ in range(n2+1)] for _ in range(n1+1)]
+        
+        for idx in range(n1+1):
+            dp[idx][n2] = 1
+        
+        for idx in range(n2):
+            dp[n1][idx] = 0
             
-            
-            
-            
+        for i in range(n1-1, -1, -1):
+            for j in range(n2-1, -1, -1):
+                if s[i] == t[j]:
+                    dp[i][j] = dp[i+1][j+1] + dp[i+1][j]
+                else:
+                    dp[i][j] = dp[i+1][j]
+        
+        return dp[0][0]
+    
+        
+        
+        
+        
+        
+        
+        
