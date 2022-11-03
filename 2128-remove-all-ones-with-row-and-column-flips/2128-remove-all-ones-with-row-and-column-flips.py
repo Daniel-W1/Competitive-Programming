3dcc86fccb45de40c80ed212009351a5ff6b9c04
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-11-03 16:58:32 +0300

    Time: 1175 ms (27.16%), Space: 16.6 MB (20.96%) - LeetHub

--- 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.py
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.py
@@ -1,19 +1,12 @@
 class Solution:
     def removeOnes(self, grid: List[List[int]]) -> bool:
-        def flipcolumn(columnidx):
-            for rowidx, row in enumerate(grid):
-                grid[rowidx][columnidx] = 1 - grid[rowidx][columnidx]
-        
-        def checkZeromatrix(grid):
-            firstrow = grid[0]
-            for idx, num in enumerate(firstrow):
-                if num != 1:
-                    flipcolumn(idx)
+        ROWS, COLS = len(grid), len(grid[0])
 
-            # check each row
-            for row in grid:
-                if len(set(row)) != 1:
-                    return False
-            return True
-        
-        return checkZeromatrix(grid)
+        for row in range(1, ROWS):
+            curr_row = ''.join(str(val) for val in grid[row])
+            prev_row = ''.join(str(val) for val in grid[row-1])
+            xor = int(curr_row, 2) ^ int(prev_row, 2)
+            # print(xor)
+            if xor != 0 and xor != (2**COLS) - 1:
+                return False
+        return True
