c5f004fed01786da6f9f9679dd114549569b8f84
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-11-09 16:14:47 +0300

    Time: 143 ms (31.83%), Space: 15.6 MB (83.19%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -7,20 +7,15 @@ class Solution:
         
         # find the least significant turned on bit
         mask = cur & -cur
-        
         answer = [0, 0]
         groups = [[], []]
+        
         # separate the numbers
         for num in nums:
             if num & mask:
-                groups[0].append(num)
+                 answer[0] ^= num
             else:
-                groups[1].append(num)
-        
-        
-        for idx, group in enumerate(groups):
-            for num in group:
-                answer[idx] ^= num
+                 answer[1] ^= num
         
         return answer
                 
