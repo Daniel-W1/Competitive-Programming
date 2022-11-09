b958d7cd3eb875cf1a9415416a252200c454861d
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-11-09 14:40:32 +0300

    Time: 62 ms (49.61%), Space: 13.9 MB (71.63%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.py
+++ 0401-binary-watch/0401-binary-watch.py
@@ -1,31 +1,12 @@
 class Solution:
     def readBinaryWatch(self, turnedOn: int) -> List[str]:
-        if turnedOn > 8:
-            return []
-      
-        ans = set()
-        def backtrack(cnt, hour, minute):
-            if cnt == 0:
-                if hour > 11 or minute > 59:
-                    return
-                hour, minute = map(str, (hour, minute))
-                if len(minute) < 2:
-                    minute = "0" + minute
-                
-                res = (hour + ":" + minute)
-                if res not in ans: ans.add(res)
-                return
-            if minute > 59 or hour > 11:
-                return
-            # choice1 
-            for bit in range(4):
-                if not hour & (1 << bit):
-                    backtrack(cnt - 1, hour|(1<<bit), minute)
-            
-            # choice2 
-            for bit in range(6):
-                if not minute & (1 << bit):
-                    backtrack(cnt - 1, hour, minute | 1<<bit)
+        ans = []
         
-        backtrack(turnedOn, 0, 0)
-        return list(ans)
+        for hour in range(12):
+            for minute in range(60):
+                current_on = bin(hour).count("1") + bin(minute).count("1")
+                if current_on == turnedOn:
+                    res = str(hour) + ":" + str(minute) if minute > 9 else str(hour) + ":" + "0"+str(minute)
+                    ans.append(res)
+        
+        return ans
