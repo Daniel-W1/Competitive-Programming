c9f78d749cceb5d8b0d681329a295f02d87c4a7e
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-11-17 16:22:07 +0300

    Time: 497 ms (77.56%), Space: 15.4 MB (99.69%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.py
+++ 1615-maximal-network-rank/1615-maximal-network-rank.py
@@ -1,57 +1,27 @@
 class Solution:
     def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
-        ans = 0
-        graph = defaultdict(list)
+        degree = defaultdict(int)
+        connected = [[False for _ in range(n)] for _ in range(n)]
+        
         for n1, n2 in roads:
-            graph[n1].append(n2)
-            graph[n2].append(n1)
+            degree[n1] += 1
+            degree[n2] += 1
+            connected[n1][n2] = True
+            connected[n2][n1] = True
         
-        visited = [False for _ in range(n)]
-        max1, max2 = 0, 0
         
+        ans = 0
         for node in range(n):
-            if not visited[node]:
-                q = deque([node])
-                visited[node]= True
-                component = []
-                max_seen = 0
-                
-                while q:
-                    for _ in range(len(q)):
-                        cur = q.popleft()
-
-                        
-                        cnt = len(graph[cur])
-                        max_seen = max(max_seen, cnt)
-                        
-                        component.append((cur, cnt))
-                        
-                            
-                        for neighbor in graph[cur]:
-                            if not visited[neighbor] :
-                                q.append((neighbor))
-                                visited[neighbor] = True
-                
-                # print(component)
-                for idx, (node, network) in enumerate(component):
-                    for idx2 in range(idx + 1, len(component)):
-                        node2, network2 = component[idx2]
-
-                        if node2 in graph[node]:
-                            ans = max(ans, network + network2 - 1)
-                        else:
-                            # print(node, node2)
-                            ans = max(ans, network + network2)
-                    
-                    # print(ans)
-
-                if max_seen > max1:
-                    max2, max1 = max1, max_seen
+            for secondnode in range(node + 1, n):
+                # print(node, secondnode)
+                if not connected[node][secondnode]:
+                    ans = max(ans, degree[node] + degree[secondnode])
                 else:
-                    max2 = max(max2, max_seen)
+                    ans = max(ans, degree[node] + degree[secondnode] - 1)
         
-        return max(ans, max1 + max2)
+        return ans
+            
         
-                            
+        
         
         
