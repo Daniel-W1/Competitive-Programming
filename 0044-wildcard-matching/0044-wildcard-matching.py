45cbd48a75ba848a7244feb1fbfb8c53d638aac1
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-11-22 20:32:16 +0300

    Time: 1740 ms (36.80%), Space: 131.4 MB (10.64%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,25 +1,19 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        
-        '''
-        ? increament both pointers
-        
-        if both are characters increment both pointers
-        
-        if * do the majic trick
-        
-        '''
-        @lru_cache(None)          
+        @cache         
         def dfs(i, j):
             if i >= len(s) and j >= len(p):
                 return True
-
+            
             if j >= len(p):
                 return False
+            
             if i < len(s) and (s[i] == p[j] or p[j] == "?"):
                 return dfs(i+1, j+1)
+            
             elif p[j] == "*":
                 return (i < len(s) and dfs(i+1, j)) or dfs(i, j+1) 
+            
             return False
         
         return dfs(0, 0)
