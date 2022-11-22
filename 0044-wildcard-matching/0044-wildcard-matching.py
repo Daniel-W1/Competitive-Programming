ce1dd6a00147122ba7d6cc038f19489b8ac87941
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-11-22 20:18:15 +0300

    Time: 1869 ms (32.42%), Space: 131.5 MB (10.38%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -13,17 +13,13 @@ class Solution:
         def dfs(i, j):
             if i >= len(s) and j >= len(p):
                 return True
-            if i >= len(s):
-                if p[j] == "*":
-                    return dfs(i, j+1)
-                return False
+
             if j >= len(p):
                 return False
             if i < len(s) and (s[i] == p[j] or p[j] == "?"):
                 return dfs(i+1, j+1)
-            print(i, j)
-            if p[j] == "*":
-                return dfs(i+1, j) or dfs(i, j+1) or dfs(i+1, j+1)
+            elif p[j] == "*":
+                return (i < len(s) and dfs(i+1, j)) or dfs(i, j+1) 
             return False
         
         return dfs(0, 0)
