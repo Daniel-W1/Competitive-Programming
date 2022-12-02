a27cf69a23ba4296a1783834ff033dbbcda355ce
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-12-02 18:36:18 +0300

    Time: 207 ms (96.83%), Space: 14.5 MB (98.94%) - LeetHub

---
+++ 1124-longest-well-performing-interval/1124-longest-well-performing-interval.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestWPI(self, hours: List[int]) -> int:
+        """
+            [9,9,6,0,6,6,9]
+            [1, 2, 1, 0, -1, -2, -1]
+        """
+        
+        prefix = {0 : -1}
+        cur_sum = 0
+        answer = 0
+        
+        for idx, num in enumerate(hours):
+            cur_sum += 1 if (num > 8) else -1
+           
+            val = 0 if cur_sum - 1 > 0 else cur_sum - 1
+            
+            if val in prefix:
+                answer = max(answer, idx - prefix[val])
+            
+            if cur_sum not in prefix:
+                prefix[cur_sum] = idx
+            # print(cur_sum, prefix, answer)
+        
+        return answer
+                
