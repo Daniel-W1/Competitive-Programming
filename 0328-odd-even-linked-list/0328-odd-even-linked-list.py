6cce36a97ce2ada2e399df3e898b25800a997701
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-12-06 21:46:37 +0300

    Time: 67 ms (72.73%), Space: 16.5 MB (78.70%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -9,21 +9,21 @@ class Solution:
         
         even_head = even_tail = ListNode()
         odd_tail = head
+        prev = None
         
         while odd_tail and odd_tail.next:
             the_next = odd_tail.next
             odd_tail.next = odd_tail.next.next
             
-            # print(odd_tail)
             the_next.next = None
             even_tail.next = the_next
             
             
             even_tail = even_tail.next
+            prev = odd_tail
             odd_tail = odd_tail.next
         
-        odd_tail = head
-        while odd_tail.next: odd_tail = odd_tail.next
+        if not odd_tail: odd_tail = prev
         odd_tail.next = even_head.next
         
         return head
