f481fcd0eb1589458bf2b2e7465dc73d0c4ff8f1
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-12-09 13:44:09 +0300

    Time: 767 ms (94.11%), Space: 17.3 MB (22.75%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,24 +1,30 @@
 class Solution:
-    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+    def minimumEffortPath(self, grid: List[List[int]]) -> int:
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        n, m = len(grid), len(grid[0])
         heap = [(0, 0, 0)]
-        ans = 0
-        direction = [(1, 0),(0,1),(-1,0),(0,-1)]
+        distance = {(r, c) : float('inf') for r in range(n) for c in range(m)}
         visited = set()
-        m, n = len(heights), len(heights[0])
-
         
         while heap:
-            diff, r, c = heappop(heap)
-            ans = max(ans, diff)
-            cur = heights[r][c]
-            visited.add((r, c))
+            dist, row, col = heappop(heap)
+            visited.add((row, col))
+            
+            if row == n-1 and col == m-1: return dist
+            
+            if distance[row, col] < dist: 
+                continue
+            
+            for dx, dy in directions:
+                new_r, new_c = row + dx, col + dy
+                
+                if 0 <= new_r < n and 0 <= new_c < m:
+                    if (new_r, new_c) not in visited:
+                        new_dist = max(dist,abs(grid[new_r][new_c] - grid[row][col]))
+                        if new_dist < distance[new_r,new_c]:
+                            distance[new_r, new_c] = new_dist
+                            heappush(heap, (new_dist, new_r, new_c))
+                            # print(heap)
+                    
+        return 0
         
-            if r == m-1 and c == n-1:
-                return ans
-            
-            for dx, dy in direction:
-                newr, newc = r+dx, c+dy
-                if 0 <= newr < m and 0 <= newc < n and (newr, newc) not in visited:
-                    heappush(heap, (abs(heights[newr][newc]-cur), newr, newc))
-
-            
