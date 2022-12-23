791c73531dbfee639851daec7a69fd06cfaf4760
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-12-23 11:00:11 +0300

    Time: 2400 ms (62.60%), Space: 21.6 MB (73.95%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.py
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -2,7 +2,7 @@ class Solution:
     def countPairs(self, deliciousness: List[int]) -> int:
         cnt = Counter()
         ans = 0
-        temp = 2 ** 40
+        temp = 2 ** 21
         mod = 10 ** 9 + 7
         
         for val in deliciousness:
@@ -11,10 +11,8 @@ class Solution:
             while total:
                 left = total - val
                 ans += cnt[left]
-                
                 total //= 2
-                
+            
             cnt[val] += 1
         
         return ans % mod
-                
