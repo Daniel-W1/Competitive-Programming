8aa500091424a6bcf3adf275dc827397fe17ae52
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2022-12-28 18:50:37 +0300

    Time: 4044 ms (5.12%), Space: 727.7 MB (5.14%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -0,0 +1,47 @@
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        
+        n = len(nums)
+        nums.sort()
+        
+        memo = {}
+        def dp(idx, prev_lcm):
+            
+            if (idx, prev_lcm) in memo: return memo[idx, prev_lcm]
+            if idx == n:
+                return 0
+            
+            cur_lcm = lcm(prev_lcm, nums[idx])
+            res = 0
+            
+            if cur_lcm == prev_lcm or cur_lcm == nums[idx]:
+                res = max(res, dp(idx+1, cur_lcm) + 1)
+            
+            res = max(res, dp(idx + 1, prev_lcm))
+            memo[idx, prev_lcm] = res
+            
+            return res
+
+        
+        max_start = -1
+        res = -1
+        for idx in range(n):
+            cur_res = dp(idx, nums[idx])
+
+            if cur_res >= res:
+                res = cur_res
+                max_start = idx
+        
+        target = res
+        prev_lcm = nums[max_start]
+        ans = []
+        
+        for idx in range(max_start, n):
+            cur = lcm(nums[idx], prev_lcm)
+            
+            if (idx, cur) in memo and memo[idx, cur] == target:
+                prev_lcm = cur
+                ans.append(nums[idx])
+                target -= 1
+        
+        return ans
