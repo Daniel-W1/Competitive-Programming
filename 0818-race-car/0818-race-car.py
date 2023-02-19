e401bc3e021d08c5bd61f855d3d071d6e27ef492
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2023-02-19 09:34:07 +0300

    Time: 49 ms (72.23%), Space: 14.4 MB (61.06%) - LeetHub

--- 0818-race-car/0818-race-car.py
+++ 0818-race-car/0818-race-car.py
@@ -1,11 +1,8 @@
 class Solution:
     def racecar(self, target: int) -> int:
-        
-        visited = set()
         q = deque([(0, 1)])
         steps = 0
-        visited.add((0, 1))
-        
+      
         while q:
             
             for _ in range(len(q)):
@@ -14,17 +11,12 @@ class Solution:
                 if cur_pos == target:
                     return steps
                 
-                if (cur_pos + cur_speed, cur_speed*2) not in visited:
-                    q.append((cur_pos + cur_speed, cur_speed*2))
-                    visited.add((cur_pos + cur_speed, cur_speed*2))
+                q.append((cur_pos + cur_speed, cur_speed*2))
                 
-                if cur_speed > 0 and (cur_pos, -1) not in visited:
-                    q.append((cur_pos, -1))
-                    visited.add((cur_pos, -1))
+                if (cur_speed > 0 and cur_pos +cur_speed > target) or (cur_pos + cur_speed < target and cur_speed < 0):
+                    direc = -1 if cur_speed > 0 else 1
+                    q.append((cur_pos, direc))
                 
-                if cur_speed < 0 and (cur_pos, 1) not in visited:
-                    q.append((cur_pos, 1))
-                    visited.add((cur_pos, 1))
             
             steps += 1
         
