8bb187558fabbdf465c6b80c0d46c9e2472f2555
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2023-02-23 19:35:39 +0300

    Time: 8591 ms (37.14%), Space: 31 MB (14.60%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -1,25 +1,29 @@
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
-        """
-            [3, 1, 5, 8]
-            
-            |   |   |
-            
-        """
+        
+        # so the idea is instead of bursting the ballon right away may be i can simulate
+        # bursting the ballon at last 
         
         nums = [1] + nums + [1]
+        
         @cache
-        def dp(left, right):
+        def dfs(left, right):
+            
             if left > right:
                 return 0
             
+            
             res = 0
-            for idx in range(left, right+1):
-                score = nums[left-1] * nums[idx] * nums[right+1]
-                score += dp(left, idx-1) + dp(idx+1, right)
+            for idx in range(left, right + 1):
+                
+                score = nums[left-1] * nums[idx] * nums[right + 1]
+                
+                score += dfs(left, idx - 1) + dfs(idx + 1, right)
                 
                 res = max(res, score)
             
             return res
         
-        return dp(1, len(nums)-2)
+        return dfs(1, len(nums)-2)
+                
+            
