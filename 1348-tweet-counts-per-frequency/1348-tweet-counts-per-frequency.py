e1b39d43ef84b9a48d32586e646972ed24a4b70a
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2023-02-24 19:42:44 +0300

    Time: 351 ms (85.45%), Space: 20.2 MB (14.55%) - LeetHub

--- 1348-tweet-counts-per-frequency/1348-tweet-counts-per-frequency.py
+++ 1348-tweet-counts-per-frequency/1348-tweet-counts-per-frequency.py
@@ -8,6 +8,7 @@ class TweetCounts:
     def recordTweet(self, tweetName: str, time: int) -> None:
         self.records[tweetName].add(time)
         # print(time, "adding ..")
+        
     def getTweetCountsPerFrequency(self, freq: str, tweetName: str, startTime: int, endTime: int) -> List[int]:
         
         inc = 86400
@@ -19,11 +20,6 @@ class TweetCounts:
         start = self.records[tweetName].bisect_right(startTime-1)
         end = self.records[tweetName].bisect_right(endTime)
         
-        if not full_rounds:
-            return [end - start]
-        
-        # print(cur_range, inc)
-        # print(tweetName, startTime, endTime, self.records)
         ans = []
         cur = min(startTime + inc - 1, endTime)
         prev = start
@@ -32,7 +28,7 @@ class TweetCounts:
             idx = self.records[tweetName].bisect_right(cur)
             ans.append(idx - prev)
             prev = idx
-            cur += (inc)
+            cur += inc
         
         if cur_range % inc:
             ans.append(end - prev)
