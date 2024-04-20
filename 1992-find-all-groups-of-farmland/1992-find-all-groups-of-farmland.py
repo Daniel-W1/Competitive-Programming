3c94e412929a14c35c5123345841fc96d13f4273
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2024-04-20 09:11:56 +0300

    Time: 783 ms (82.50%), Space: 31.2 MB (49.00%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -1,28 +1,18 @@
 class Solution:
     def findFarmland(self, grid: List[List[int]]) -> List[List[int]]:
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         ans = []
         
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == 1:
-                    q = deque([(r, c)])
-                    grid[r][c] = 0
                     
-                    end = [r, c]
-                    
-                    while q:
-                        row, col = q.popleft()
+                    for row in range(r, len(grid)+1):
+                        if not row < len(grid) or not grid[row][c]: break
                         
-                        for dx, dy in directions:
-                            new_r, new_c = row + dx, col + dy
-                        
-                            if 0 <= new_r < len(grid) and 0 <= new_c < len(grid[0]) and grid[new_r][new_c] == 1:
-                                q.append((new_r, new_c))
-                                grid[new_r][new_c] = 0
-                                end[0] = max(end[0], new_r)
-                                end[1] = max(end[1], new_c)
+                        for col in range(c, len(grid[0])+1):
+                            if not col < len(grid[0]) or not grid[row][col]: break
+                            grid[row][col] = 0
                 
-                    ans.append([r, c, end[0], end[1]])
+                    ans.append([r, c, row-1, col-1])
             
         return ans
