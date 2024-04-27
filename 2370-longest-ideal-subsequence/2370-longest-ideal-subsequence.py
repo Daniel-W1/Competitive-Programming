1a2d1c7488c586ba26941863476fb9bd56f9cfe4
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2024-04-27 16:44:35 +0300

    Time: 1850 ms (17.05%), Space: 17.4 MB (30.16%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
@@ -6,14 +6,15 @@ class Solution:
             1,2,1,2,3, 4  
         """
         prev_chars = [0] * 26
-        dp = [0] * len(s)
         
         for idx, char in enumerate(s):
             pos_idx = ord(char) - 97
+            best = 0
+            
             for p_idx, val in enumerate(prev_chars):
                 if pos_idx - k <= p_idx <= pos_idx + k:
-                    dp[idx] = max(dp[idx], prev_chars[p_idx] + 1)
+                    best = max(best, prev_chars[p_idx] + 1)
             
-            prev_chars[pos_idx] = dp[idx]
-        
-        return max(dp)
+            prev_chars[pos_idx] = best
+            
+        return max(prev_chars)
