13e7abdc83169963ae2f9e15d870538d9193c41b
Daniel <82764803+Daniel-W1@users.noreply.github.com>
2024-04-27 16:10:36 +0300

    Time: 418 ms (35.75%), Space: 26.8 MB (63.98%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,34 +1,35 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        if n <= 2:
-            return [i for i in range(n)]
+        if  n == 1: return [0]
         
-        graph = defaultdict(set)
+        graph = defaultdict(list)
+        indegree = defaultdict(int)
         
-        for n1, n2 in edges:
-            graph[n1].add(n2)
-            graph[n2].add(n1)
-            
-        # collect the leaves
-        leaves = deque([])
-        remaining = n
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+            indegree[a] += 1
+            indegree[b] += 1
+        
+        leaves = []
         for node in graph:
-            if len(graph[node]) == 1:
+            if indegree[node] == 1:
                 leaves.append(node)
-                
-        while remaining > 2:
-            remaining -= len(leaves)
-            newleaves = deque([])
+                n -= 1
+        
+        while n:
+            new_leaves = []
             
             while leaves:
-                cur = leaves.popleft()
-                
-                for neigh in graph[cur]:
-                    graph[neigh].remove(cur)
-                    
-                    if len(graph[neigh]) == 1:
-                        newleaves.append(neigh)
-            leaves = newleaves
+                cur_node = leaves.pop()
+
+                for neigh in graph[cur_node]:
+                    indegree[neigh] -= 1
+
+                    if indegree[neigh] == 1:
+                        new_leaves.append(neigh)
+                        n -= 1
+            
+            leaves = new_leaves[:]
         
         return leaves
-            
